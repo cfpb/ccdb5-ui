4489af115df703fff5e29a3deea557db3dae84fd
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-14 13:09:25 -0400

    Provide correct attribute for "for"

--- src/Filters/SingleCheckbox.jsx
+++ src/Filters/SingleCheckbox.jsx
@@ -7,7 +7,7 @@ export default class SingleCheckbox extends React.Component {
         <h5>{this.props.title}</h5>
         <div className="m-form-field m-form-field__checkbox">
             <input className="a-checkbox" type="checkbox" id="theCheckbox" />
-            <label className="a-label" for="theCheckbox">{this.props.label}</label>
+            <label className="a-label" htmlFor="theCheckbox">{this.props.label}</label>
         </div> 
       </section>
     );
