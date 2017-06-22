1e74d89794630831e759c4a99fc711ee4fec4450
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-22 13:49:07 -0400

    Trigger an API call when the page changes

--- src/actions/__tests__/paging.spec.jsx
+++ src/actions/__tests__/paging.spec.jsx
@@ -1,14 +1,34 @@
-import { changePage } from '../paging'
+jest.mock('../complaints');
+
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { changePage, pageChanged } from '../paging'
 import * as types from '../../constants'
 
 describe('action:paging', () => {
-  describe('changePage', () => {
+  describe('pageChanged', () => {
     it('creates a simple action', () => {
         const expectedAction = {
           type: types.PAGE_CHANGED,
           page: 99
         }
-        expect(changePage(99)).toEqual(expectedAction)
+        expect(pageChanged(99)).toEqual(expectedAction)
+    })
+  })
+
+  describe('changePage', () => {
+    it('executes a chain of actions', () => {
+      const expectedActions = [
+        { type: types.PAGE_CHANGED, page: 99 },
+        { type: 'getComplaintsMock' }
+      ]
+
+      const middlewares = [thunk]
+      const mockStore = configureMockStore(middlewares)
+      const store = mockStore({ })
+
+      store.dispatch(changePage(99))
+      expect(store.getActions()).toEqual(expectedActions)
     })
   })
 })
