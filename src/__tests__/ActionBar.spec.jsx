6c44cfd589baf23438463a4e3872f956d83e9e50
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-26 16:10:52 -0400

    Show different title if hits != doc_count

--- src/__tests__/ActionBar.spec.jsx
+++ src/__tests__/ActionBar.spec.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ActionBar from '../ActionBar';
+import { IntlProvider } from 'react-intl';
+import { ActionBar} from '../ActionBar';
 import renderer from 'react-test-renderer';
 
 describe('initial state', () => {
   it('renders without crashing', () => {
     const target = renderer.create(
-      <ActionBar />
+      <IntlProvider locale="en">
+        <ActionBar total="100" hits="10" />
+      </IntlProvider>
     );
 
     let tree = target.toJSON();
