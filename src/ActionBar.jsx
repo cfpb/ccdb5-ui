6c787aa33fc3bdb5336e1393b08d036be4201d95
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-26 16:10:52 -0400

    Show different title if hits != doc_count

--- src/ActionBar.jsx
+++ src/ActionBar.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { FormattedNumber } from 'react-intl';
 import './ActionBar.less';
 
-export default class ActionBar extends React.Component {
+export class ActionBar extends React.Component {
   render() {
     return (
         <summary className="action-bar">
-          <div><h2>Showing x of y complaints</h2></div>
+          <div>{ this.props.hits === this.props.total
+            ? (<h2>
+                 Showing&nbsp;
+                 <FormattedNumber value={this.props.total} />
+                 &nbsp;total complaints
+              </h2>)
+            : (<h2>
+                Showing&nbsp;
+                <FormattedNumber value={this.props.hits} />
+                &nbsp;matches out of&nbsp;
+                <FormattedNumber value={this.props.total} />
+                &nbsp;total complaints</h2>)
+          }
+          </div>
           <div>Page | Sort | Export</div>
         </summary>
     );
   }
 }
+
+export const mapStateToProps = state => {
+  return {
+    hits: state.results.total,
+    total: state.results.doc_count
+  }
+}
+
+export const mapDispatchToProps = dispatch => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ActionBar)
