b106110100045166ba37d9370205a212b3185cc5
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-27 10:17:47 -0400

    Implement the wireframe for the action bar

--- src/ActionBar.jsx
+++ src/ActionBar.jsx
@@ -21,7 +21,26 @@ export class ActionBar extends React.Component {
                 &nbsp;total complaints</h2>)
           }
           </div>
-          <div>Page | Sort | Export</div>
+          <div className="layout-row">
+            <div className="cf-select">
+              <select id="choose-size">
+                <option value="10">Show 10 results</option>
+                <option value="25">Show 25 results</option>
+                <option value="50">Show 50 results</option>
+                <option value="100">Show 100 results</option>
+              </select>
+            </div>
+            <div className="cf-select">
+              <select id="choose-sort">
+                <option value="relevance_asc">Sort by relevance</option>
+                <option value="relevance_desc">Sort by relevance (desc)</option>
+                <option value="date_asc">Sort by oldest to newest</option>
+                <option value="date_desc">Sort by newest to oldest</option>
+              </select>
+            </div>
+
+            <h5 className="flex-all"><a>Export results</a></h5>
+          </div>
         </summary>
     );
   }
