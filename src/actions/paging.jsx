bf9148538f393f09ad645780e2a3facaba648a88
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-27 11:07:23 -0400

    Add actions when changing size or sort

--- src/actions/paging.jsx
+++ src/actions/paging.jsx
@@ -1,5 +1,8 @@
-import { PAGE_CHANGED } from '../constants'
+import { PAGE_CHANGED, SIZE_CHANGED, SORT_CHANGED } from '../constants'
 import { getComplaints } from './complaints'
+
+// ----------------------------------------------------------------------------
+// Simple actions
 
 export function pageChanged(page) {
   return {
@@ -8,9 +11,40 @@ export function pageChanged(page) {
   }
 }
 
+export function sizeChanged(size) {
+  return {
+    type: SIZE_CHANGED,
+    size
+  }
+}
+
+export function sortChanged(sort) {
+  return {
+    type: SORT_CHANGED,
+    sort
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Compound actions
+
 export function changePage(page) {
   return dispatch => {
       dispatch(pageChanged(page))
+      dispatch(getComplaints())
+  }
+}
+
+export function changeSize(size) {
+  return dispatch => {
+      dispatch(sizeChanged(size))
+      dispatch(getComplaints())
+  }
+}
+
+export function changeSort(sort) {
+  return dispatch => {
+      dispatch(sortChanged(sort))
       dispatch(getComplaints())
   }
 }
