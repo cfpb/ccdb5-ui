1635ffeade7f245cea6883ad341d9454a9d9bf0c
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-29 15:21:00 -0400

    Adding constants used by the filter pills

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -8,3 +8,21 @@ export const URL_CHANGED               = 'URL_CHANGED'
 // External Triggers
 export const COMPLAINTS_RECEIVED       = 'COMPLAINTS_RECEIVED'
 export const COMPLAINTS_FAILED         = 'COMPLAINTS_FAILED'
+
+// Useful constants
+export const knownFilters = [
+  'company',
+  'company_public_response',
+  'company_response',
+  'consumer_consent_provided',
+  'consumer_disputed',
+  'issue',
+  'product',
+  'state',
+  'submitted_via',
+  'tag',
+  'timely',
+  'zip_code'
+]
+
+export const SLUG_SEPARATOR            = '•'
