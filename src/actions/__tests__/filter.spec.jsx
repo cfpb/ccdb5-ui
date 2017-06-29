c22921398e5f57808ea978bd4c8bb2fb39b18dd6
sephcoster <sephcoster@gmail.com>
2017-06-29 12:42:38 -0400

    Added AggItem Spec, filter spec scaffold

---
+++ src/actions/__tests__/filter.spec.jsx
@@ -0,0 +1,10 @@
+jest.mock('../complaints');
+
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import filter, { filterChanged } from '../search'
+import * as types from '../../constants'
+
+describe('action:filter', () => {
+
+})
