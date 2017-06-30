d71ed2159d0cc9c54842b2f7e9c68a74e602a6ba
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-06-30 16:07:34 -0400

    Implement filter pill actions

--- src/actions/filter.jsx
+++ src/actions/filter.jsx
@@ -1,7 +1,9 @@
-import { FILTER_CHANGED } from '../constants'
+import { FILTER_CHANGED, FILTER_REMOVED, FILTER_ALL_REMOVED } from '../constants'
 import { getComplaints } from './complaints'
 
-// Simple Action
+// ----------------------------------------------------------------------------
+// Simple actions
+
 export function filterToggle(filterName, filterValue) {
   return {
     type: FILTER_CHANGED,
@@ -10,10 +12,40 @@ export function filterToggle(filterName, filterValue) {
   }
 }
 
-// Compound Action
+export function filterRemoved(filterName, filterValue) {
+  return {
+    type: FILTER_REMOVED,
+    filterName,
+    filterValue
+  }
+}
+
+export function filterAllRemoved() {
+  return {
+    type: FILTER_ALL_REMOVED
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Compound Actions
+
 export function filterChanged(filterName, filterValue) {
   return dispatch => {
       dispatch( filterToggle(filterName, filterValue) )
+      dispatch( getComplaints() )
+  }
+}
+
+export function removeFilter(filterName, filterValue) {
+  return dispatch => {
+      dispatch( filterRemoved(filterName, filterValue) )
+      dispatch( getComplaints() )
+  }
+}
+
+export function removeAllFilters() {
+  return dispatch => {
+      dispatch( filterAllRemoved() )
       dispatch( getComplaints() )
   }
 }
