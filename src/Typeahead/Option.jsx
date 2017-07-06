087916aaeec280e0a2eb0d9746dd756dfa0dba35
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-07-06 11:28:00 -0400

    Start implementing typeahead

---
+++ src/Typeahead/Option.jsx
@@ -0,0 +1,25 @@
+// Adapted from https://github.com/fmoo/react-typeahead
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default class Option extends React.Component {
+  render() {
+    return (
+     <li className="typeahead-option"
+         onClick={this.props.onClick}>
+       { this.props.children }
+     </li>
+    )
+  }
+}
+
+Option.propTypes = {
+  onClick: PropTypes.func
+}
+
+Option.defaultProps = {
+  onClick: function(event) {
+    event.preventDefault();
+  }
+}
