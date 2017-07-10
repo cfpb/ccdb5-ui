be27734054f573584a5ee49e0642f305cd4fa446
sephcoster <sephcoster@gmail.com>
2017-07-10 11:26:21 -0400

    wip more with testing

--- src/Filters/Aggregation.jsx
+++ src/Filters/Aggregation.jsx
@@ -5,6 +5,18 @@ import AggregationItem from './AggregationItem';
 import './Aggregation.less';
 
 export class Aggregation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMore: this.props.showMore || false }
+    this._toggleShowMore = this._toggleShowMore.bind(this);
+  }
+
+  _toggleShowMore() {
+    this.setState({
+      showMore: !this.state.showMore
+    })
+  }
+
   render() {
     const all = this.props.options || [];
     const some = all.length > 6 ? all.slice(0, 5) : all
@@ -16,13 +28,22 @@ export class Aggregation extends React.Component {
                            showChildren={this.props.showChildren}
                            className="aggregation">
             <ul>
-            {some.map(bucket =>
+            {!this.state.showMore ?
+              some.map(bucket =>
+                <AggregationItem item={bucket} key={ bucket.key } fieldName={ this.props.fieldName } />
+            ) :
+              all.map(bucket =>
                 <AggregationItem item={bucket} key={ bucket.key } fieldName={ this.props.fieldName } />
             )}
             </ul>
-            {remain > 0 ? (
+            {remain > 0 && !this.state.showMore ? (
               <div className="flex-fixed">
-                   <button className="a-btn a-btn__link hover more">+ Show {remain} more</button>
+                   <button className="a-btn a-btn__link hover more" onClick={ this._toggleShowMore }>+ Show {remain} more</button>
+              </div>
+            ) : null}
+            {remain > 0 && this.state.showMore ? (
+              <div className="flex-fixed">
+                   <button className="a-btn a-btn__link hover more" onClick={ this._toggleShowMore }>- Show {remain} less </button>
               </div>
             ) : null}
         </CollapsibleFilter>
