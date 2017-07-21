f554544b3d170ee8650fc050860ab75390e95855
sephcoster <sephcoster@gmail.com>
2017-07-21 14:22:27 -0400

    Updated tests to work with router

--- src/__tests__/App.spec.jsx
+++ src/__tests__/App.spec.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { App } from '../App';
 import { defaultQuery } from '../reducers/query'
 import renderer from 'react-test-renderer';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 describe('initial state', () => {
   it('renders without crashing', () => {
