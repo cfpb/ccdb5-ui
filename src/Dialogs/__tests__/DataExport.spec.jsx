fc656e306a9869009756960b7862e3c127f7671e
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-07-25 16:15:16 -0400

    Add functionality

--- src/Dialogs/__tests__/DataExport.spec.jsx
+++ src/Dialogs/__tests__/DataExport.spec.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
-import DataExport from '../DataExport'
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import ReduxDataExport from '../DataExport'
+
+function setupSnapshot() {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+  const store = mockStore({
+    results: {
+      doc_count: 9999,
+      total: 1001
+    },
+  })
+
+  return renderer.create(
+    <Provider store={store}>
+      <IntlProvider locale="en">
+        <ReduxDataExport />
+      </IntlProvider>
+    </Provider>
+  )
+}
 
 describe('component::DataExport', () => {
   describe('initial state', () => {
     it('renders without crashing', () => {
-      const target = renderer.create(
-        <DataExport />
-      )
-
+      const target = setupSnapshot()
       const tree = target.toJSON()
       expect(tree).toMatchSnapshot()
     })
--- src/actions/dataExport.jsx
+++ src/actions/dataExport.jsx
@@ -1,6 +1,9 @@
-// Tip of the hat to: https://stackoverflow.com/questions/35623656
+// Tip of the hat to: 
+// https://stackoverflow.com/questions/35623656
+// https://stackoverflow.com/questions/3916191
 
-import { MODAL_SHOWN, MODAL_TYPE_DATA_EXPORT } from '../constants'
+import { stateToQS } from './complaints'
+import { MODAL_HID, MODAL_SHOWN, MODAL_TYPE_DATA_EXPORT } from '../constants'
 
 export function showExportDialog() {
   return {
@@ -8,4 +11,34 @@ export function showExportDialog() {
     modalType: MODAL_TYPE_DATA_EXPORT,
     modalProps: {}
   }
+}
+
+export function exportResults(format, size) {
+  return (dispatch, getState) => {
+    dispatch({type: MODAL_HID})
+
+    const params = {...getState()}
+    params.query.size = 1000
+    const uri = '@@API' + stateToQS(params)
+    const link = document.createElement("a")
+    link.href = uri
+    link.download = 'monkey.csv'
+    //link.target = '_blank'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }  
+}
+
+export function visitSocrata() {
+  return dispatch => {
+    dispatch({type: MODAL_HID})
+
+    const link = document.createElement("a")
+    link.href = 'https://data.consumerfinance.gov/dataset/Consumer-Complaints/s6ew-h6mp'
+    link.target = '_blank'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+  }  
 }
