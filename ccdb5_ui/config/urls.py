068def7a611ce31984142b513794790f20d641d1
sephcoster <sephcoster@gmail.com>
2017-07-28 17:55:13 -0400

    Removed commented route

--- ccdb5_ui/config/urls.py
+++ ccdb5_ui/config/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from ccdb5_ui.views import CCDB5MainView
 
 urlpatterns = [
-    # url(r'^/\d+', CCDB5MainView.as_view()),
     url(r'^.*', CCDB5MainView.as_view()),
 ]
