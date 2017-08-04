639d2a6f8df8dcc66e491f50c2adf208d17785d8
AdamZarger <adam.zarger@cfpb.gov>
2017-08-04 08:54:37 -0400

    Added tests for SimpleFilter and SimpleCollapsibleFilter

---
+++ src/Filters/__tests__/SimpleFilter.spec.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { IntlProvider } from 'react-intl';
+import ReduxSimpleFilter from '../SimpleFilter';
+import renderer from 'react-test-renderer';
+
+function setupSnapshot(initialAggs) {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+  const store = mockStore({
+    query: {},
+    aggs: {
+      company_response: initialAggs
+    }
+  })
+
+  return renderer.create(
+    <Provider store={store}>
+      <IntlProvider locale="en">
+        <ReduxSimpleFilter fieldName="company_response"/>
+      </IntlProvider>
+    </Provider>
+  )
+}
+
+describe('initial state', () => {
+  it('renders without crashing', () => {
+    const target = setupSnapshot([
+      {key: 'foo', doc_count: 99}
+    ]);
+    const tree = target.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
