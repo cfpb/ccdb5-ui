dcfa0a276562b14a8e3ca53e90f42cde77d31d7b
AdamZarger <adam.zarger@cfpb.gov>
2017-08-07 16:00:34 -0400

    Initial attempt at Narrative filter

--- src/Filters/SingleCheckbox.jsx
+++ src/Filters/SingleCheckbox.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import AggregationItem from './AggregationItem'
+import React from 'react'
+import SingleCheckboxOption from './SingleCheckboxOption'
 
 export default class SingleCheckbox extends React.Component {
   render() {
+    const listComponentProps = {
+      fieldName: this.props.fieldName
+    }
+
     return (
       <section className="single-checkbox">
         <h5>{this.props.title}</h5>
         <div className="m-form-field m-form-field__checkbox">
-            <input className="a-checkbox" type="checkbox" id="theCheckbox" />
-            <label className="a-label" htmlFor="theCheckbox">{this.props.label}</label>
-        </div> 
+            <ul>
+              <SingleCheckboxOption listComponent={AggregationItem}
+                                    listComponentProps={listComponentProps}
+                                    options={this.props.options}
+              />
+            </ul>
+        </div>
       </section>
     );
   }
