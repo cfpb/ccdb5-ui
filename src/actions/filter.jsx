183e2ae14ee50eb985d4d6e98d0e3b0e977eb875
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-08-08 09:56:32 -0400

    Comply with Lint rules

--- src/actions/filter.jsx
+++ src/actions/filter.jsx
@@ -1,12 +1,19 @@
 import {
-  FILTER_ALL_REMOVED, FILTER_CHANGED, FILTER_MULTIPLE_ADDED, FILTER_MULTIPLE_REMOVED,
-  FILTER_REMOVED
+  FILTER_ALL_REMOVED, FILTER_CHANGED, FILTER_MULTIPLE_ADDED,
+  FILTER_MULTIPLE_REMOVED, FILTER_REMOVED
 } from '../constants'
 import { getComplaints } from './complaints'
 
 // ----------------------------------------------------------------------------
 // Simple actions
 
+/**
+* Creates an action when an aggregation filter checkbox is clicked
+*
+* @param {string} filterName which filter was clicked
+* @param {string} filterValue the value of the filter that was clicked
+* @returns {string} a packaged payload to be used by Redux reducers
+*/
 export function filterToggle( filterName, filterValue ) {
   return {
     type: FILTER_CHANGED,
@@ -15,6 +22,13 @@ export function filterToggle( filterName, filterValue ) {
   }
 }
 
+/**
+* Creates an action to remove a filter
+*
+* @param {string} filterName which filter was clicked
+* @param {string} filterValue the value of the filter that was clicked
+* @returns {string} a packaged payload to be used by Redux reducers
+*/
 export function filterRemoved( filterName, filterValue ) {
   return {
     type: FILTER_REMOVED,
@@ -23,13 +37,26 @@ export function filterRemoved( filterName, filterValue ) {
   }
 }
 
+/**
+* Creates an action to remove all filters
+*
+* @returns {string} a packaged payload to be used by Redux reducers
+*/
 export function filterAllRemoved() {
   return {
     type: FILTER_ALL_REMOVED
   }
 }
 
+/**
+* Creates an action to add several related filters
+*
+* @param {string} filterName which filter is being updated
+* @param {array} values one or more values to add to the filter set
+* @returns {string} a packaged payload to be used by Redux reducers
+*/
 export function filterMultipleAdded( filterName, values ) {
+  // eslint-disable-next-line no-console
   console.assert( Array.isArray( values ) )
   return {
     type: FILTER_MULTIPLE_ADDED,
@@ -38,7 +65,15 @@ export function filterMultipleAdded( filterName, values ) {
   }
 }
 
+/**
+* Creates an action to remove several related filters
+*
+* @param {string} filterName which filter is being updated
+* @param {array} values one or more values to remove to the filter set
+* @returns {string} a packaged payload to be used by Redux reducers
+*/
 export function filterMultipleRemoved( filterName, values ) {
+  // eslint-disable-next-line no-console
   console.assert( Array.isArray( values ) )
   return {
     type: FILTER_MULTIPLE_REMOVED,
@@ -50,6 +85,13 @@ export function filterMultipleRemoved( filterName, values ) {
 // ----------------------------------------------------------------------------
 // Compound Actions
 
+/**
+* Adds several filters to the current set
+*
+* @param {string} filterName which filter is being updated
+* @param {array} values one or more values to add to the filter set
+* @returns {function} a series of actions to execute
+*/
 export function addMultipleFilters( filterName, values ) {
   return dispatch => {
     dispatch( filterMultipleAdded( filterName, values ) )
@@ -57,6 +99,13 @@ export function addMultipleFilters( filterName, values ) {
   }
 }
 
+/**
+* Changes one filter in the current set
+*
+* @param {string} filterName which filter is being updated
+* @param {string} filterValue the value being changed
+* @returns {function} a series of actions to execute
+*/
 export function filterChanged( filterName, filterValue ) {
   return dispatch => {
     dispatch( filterToggle( filterName, filterValue ) )
@@ -64,6 +113,13 @@ export function filterChanged( filterName, filterValue ) {
   }
 }
 
+/**
+* Removes one filter from the current set
+*
+* @param {string} filterName which filter is being updated
+* @param {string} filterValue the value being removed
+* @returns {function} a series of actions to execute
+*/
 export function removeFilter( filterName, filterValue ) {
   return dispatch => {
     dispatch( filterRemoved( filterName, filterValue ) )
@@ -71,6 +127,11 @@ export function removeFilter( filterName, filterValue ) {
   }
 }
 
+/**
+* Remove all filters from the current set
+*
+* @returns {function} a series of actions to execute
+*/
 export function removeAllFilters() {
   return dispatch => {
     dispatch( filterAllRemoved() )
@@ -78,6 +139,13 @@ export function removeAllFilters() {
   }
 }
 
+/**
+* Removes several filters from the current set
+*
+* @param {string} filterName which filter is being updated
+* @param {array} values one or more values to remove from the filter set
+* @returns {function} a series of actions to execute
+*/
 export function removeMultipleFilters( filterName, values ) {
   return dispatch => {
     dispatch( filterMultipleRemoved( filterName, values ) )
