2018a3eb26a66184ad161028a7be3be50cd105cc
AdamZarger <adam.zarger@cfpb.gov>
2017-08-10 15:40:28 -0400

    Fixed broken tests

--- src/Filters/__tests__/SingleCheckBox.spec.jsx
+++ src/Filters/__tests__/SingleCheckBox.spec.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import SingleCheckbox from '../SingleCheckbox';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import { Provider } from 'react-redux'
+import configureMockStore from 'redux-mock-store'
+import SingleCheckbox from '../SingleCheckbox'
+import renderer from 'react-test-renderer'
+import thunk from 'redux-thunk'
+
+function setupSnapshot() {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+  const store = mockStore({
+    query: {},
+    aggs: {}
+  })
+
+  return renderer.create(
+    <Provider store={store}>
+      <SingleCheckbox />
+    </Provider>
+  )
+}
 
 describe('initial state', () => {
   it('renders without crashing', () => {
-    const target = renderer.create(
-      <SingleCheckbox title="foo" label="bar" />
-    );
-
-    let tree = target.toJSON();
-    expect(tree).toMatchSnapshot();
+    const target = setupSnapshot()
+    const tree = target.toJSON()
+    expect(tree).toMatchSnapshot()
   });
 });
 
