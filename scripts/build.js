eed4588ef70de8ebae85e4610a7ee2b2d0c24121
Jeff Farley <JeffreyMFarley@users.noreply.github.com>
2017-08-15 15:30:41 -0400

    Allow linter warnings in release build

--- scripts/build.js
+++ scripts/build.js
@@ -65,11 +65,6 @@ function build( previousFileSizes ) {
       process.exit( 1 );
     }
 
-    if ( process.env.CI && stats.compilation.warnings.length ) {
-      printErrors( 'Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.', stats.compilation.warnings );
-      process.exit( 1 );
-    }
-
     console.log( chalk.green( 'Compiled successfully.' ) );
     console.log();
 
