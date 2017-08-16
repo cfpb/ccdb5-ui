41d6bc98ae30012090a7385668530a941c6c8871
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-08-16 13:56:00 -0400

    Extract deploy logic to shell file

---
+++ .travis/pu.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "cfpbdeploybot@cfpb.gov"
+  git config --global user.name "cfpbdeploybot"
+  git config --global push.default matching
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
+}
+
+make_version() {
+  npm version patch -m "[skip ci] Releasing, upgrading the version to %s"
+  # git tag <tag-name> -a -f -m "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git push origin HEAD:$TRAVIS_BRANCH
+  git push --tags
+}
+
+setup_git
+make_version
+upload_files
