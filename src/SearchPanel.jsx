7f3e2eb5527f11ce58725e66bd7ac94c46fd0ab9
Adam Zarger <adam.zarger@cfpb.gov>
2017-09-21 15:15:23 -0400

    testing commits to local environment

--- src/SearchPanel.jsx
+++ src/SearchPanel.jsx
@@ -3,12 +3,15 @@ import PillPanel from './PillPanel'
 import React from 'react';
 import SearchBar from './SearchBar'
 
-export const SearchPanel = () =>
-    <div className="search-panel">
-      <h2>Search complaint data</h2>
-      <SearchBar />
-      <PillPanel />
-    </div>
-
-
+export class SearchPanel extends React.Component {
+  render() {
+    return (
+      <div className="search-panel">
+        <h2>Search complaint data AHHHHHH</h2>
+        <SearchBar />
+        <PillPanel />
+      </div>
+    )
+  }
+}
 export default SearchPanel
