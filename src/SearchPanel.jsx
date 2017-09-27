1a89ca81d4d2b03b46be79743a9706788f4740ae
Adam Zarger <adam.zarger@cfpb.gov>
2017-09-27 11:27:52 -0400

    Cleaned up formatting

--- src/SearchPanel.jsx
+++ src/SearchPanel.jsx
@@ -8,24 +8,24 @@ import { shortIsoFormat } from './utils'
 
 export class SearchPanel extends React.Component {
   render() {
-    if (this.props.lastUpdated) {
+    if ( this.props.lastUpdated ) {
       var shortDate = shortIsoFormat( new Date( this.props.lastUpdated ) );
       return (
-	      <div className="search-panel">
-	        <h2>Search complaint data <span className="date-subscript">(last updated: <FormattedDate value={ shortDate } />)</span></h2>
-	        <SearchBar />
-	        <PillPanel />
-	      </div>
-	    )
-    } else {
-    	return (
-	      <div className="search-panel">
-	        <h2>Search complaint data</h2>
-	        <SearchBar />
-	        <PillPanel />
-	      </div>
-	    )
+          <div className="search-panel">
+            <h2>Search complaint data <span className="date-subscript">(last updated: <FormattedDate value={ shortDate } />)</span></h2>
+            <SearchBar />
+            <PillPanel />
+          </div>
+      )
     }
+
+    return (
+      <div className="search-panel">
+        <h2>Search complaint data</h2>
+        <SearchBar />
+        <PillPanel />
+      </div>
+    )
   }
 }
 
