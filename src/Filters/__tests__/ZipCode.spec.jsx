3201eb603fb92f3f916ebc2551d1d3cbf9f50470
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-10-06 10:07:32 -0400

    test: Add tests for typeahead callouts

--- src/Filters/__tests__/ZipCode.spec.jsx
+++ src/Filters/__tests__/ZipCode.spec.jsx
@@ -18,6 +18,7 @@ function setupEnzyme() {
   const props = {
     forTypeahead: [],
     options: [],
+    queryString: '?foo=bar&baz=qaz',
     typeaheadSelect: jest.fn()
   }
 
@@ -59,11 +60,56 @@ describe('component::ZipCode', () => {
     })
   })
 
-  describe('_onOptionSelected', () => {
-    it('checks the filter associated with the option', () => {
-      const {target, props} = setupEnzyme()
-      target.instance()._onOptionSelected({key: 'foo'})
-      expect(props.typeaheadSelect).toHaveBeenCalledWith('foo')
+  describe('Typeahead interface', () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockImplementation((url) => {
+        expect(url).toContain('@@API_suggest_zip/?foo=bar&baz=qaz&text=')
+
+        return new Promise((resolve) => {
+          resolve({
+            json: function() {
+              return ['foo', 'bar', 'baz', 'qaz']
+            }
+          })
+        })
+      })
+    })
+
+    describe('_onInputChange', () => {
+      it('provides a promise', () => {
+        const {target} = setupEnzyme()
+        const actual = target.instance()._onInputChange('20')
+        expect(actual.then).toBeInstanceOf(Function)
+      })
+    })
+
+    describe('_renderOption', () => {
+      it('produces a custom component', () => {
+        const {target, props} = setupEnzyme()
+        const option = {
+          key: 'Foo',
+          label: 'foo',
+          position: 0,
+          value: 'FOO'
+        }
+        const actual = target.instance()._renderOption(option)
+        expect(actual.value).toEqual('Foo')
+        expect(actual.component).toMatchObject({
+          props: {
+            label: 'foo',
+            position: 0,
+            value: 'FOO'
+          }
+        })
+      })
+    })
+  
+    describe('_onOptionSelected', () => {
+      it('checks the filter associated with the option', () => {
+        const {target, props} = setupEnzyme()
+        target.instance()._onOptionSelected({key: 'foo'})
+        expect(props.typeaheadSelect).toHaveBeenCalledWith('foo')
+      })
     })
   })
 
