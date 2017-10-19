21a95e21e8dedbc9fefea0c997de80b70b98fcc9
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-10-19 15:13:59 -0400

    chore: Don't use chunked files in dev either

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -136,7 +136,7 @@ module.exports = {
             loader: require.resolve('url-loader'),
             options: {
               limit: 10000,
-              name: 'static/media/[name].[hash:8].[ext]',
+              name: 'static/media/[name].[ext]',
             },
           },
           // Process JS with Babel.
@@ -204,21 +204,17 @@ module.exports = {
               require.resolve('less-loader')
             ],
           },
-          // "file" loader makes sure those assets get served by WebpackDevServer.
-          // When you `import` an asset, you get its (virtual) filename.
-          // In production, they would get copied to the `build` folder.
           // This loader doesn't use a "test" so it will catch all modules
           // that fall through the other loaders.
           {
+            loader: require.resolve('ignore-loader'),
             // Exclude `js` files to keep "css" loader working as it injects
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.js$/, /\.html$/, /\.json$/],
-            loader: require.resolve('file-loader'),
-            options: {
-              name: 'static/media/[name].[hash:8].[ext]',
-            },
+            exclude: [
+              /\.js$/, /\.html$/
+            ]
           },
         ],
       },
