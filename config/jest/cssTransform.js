ad608a4672b16595067510f49e9793a8283d7d60
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-10-19 10:47:41 -0400

    chore: Use updated scripts [skip ci]
    These are from create-react-app v1.4 which supports webpack 3

--- config/jest/cssTransform.js
+++ config/jest/cssTransform.js
@@ -1,3 +1,5 @@
+
+
 // This is a custom Jest transformer turning style imports into empty objects.
 // http://facebook.github.io/jest/docs/tutorial-webpack.html
 
@@ -8,5 +10,5 @@ module.exports = {
   getCacheKey() {
     // The output is always the same.
     return 'cssTransform';
-  }
+  },
 };
--- config/polyfills.js
+++ config/polyfills.js
@@ -1,73 +1,16 @@
-if ( typeof Promise === 'undefined' ) {
+
+
+if (typeof Promise === 'undefined') {
   // Rejection tracking prevents a common issue where React gets into an
   // inconsistent state due to an error, but it gets swallowed by a Promise,
   // and the user has no idea what causes React's erratic future behavior.
-  require( 'promise/lib/rejection-tracking' ).enable();
-  window.Promise = require( 'promise/lib/es6-extensions.js' );
+  require('promise/lib/rejection-tracking').enable();
+  window.Promise = require('promise/lib/es6-extensions.js');
 }
 
 // fetch() polyfill for making API calls.
-require( 'whatwg-fetch' );
+require('whatwg-fetch');
 
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
-Object.assign = require( 'object-assign' );
-
-// https://tc39.github.io/ecma262/#sec-array.prototype.includes
-if (!Array.prototype.includes) {
-  Object.defineProperty(Array.prototype, 'includes', {
-    value: function(searchElement, fromIndex) {
-
-      // 1. Let O be ? ToObject(this value).
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
-
-      var o = Object(this);
-
-      // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
-
-      // 3. If len is 0, return false.
-      if (len === 0) {
-        return false;
-      }
-
-      // 4. Let n be ? ToInteger(fromIndex).
-      //    (If fromIndex is undefined, this step produces the value 0.)
-      var n = fromIndex | 0;
-
-      // 5. If n ≥ 0, then
-      //  a. Let k be n.
-      // 6. Else n < 0,
-      //  a. Let k be len + n.
-      //  b. If k < 0, let k be 0.
-      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-      function sameValueZero(x, y) {
-        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
-      }
-
-      // 7. Repeat, while k < len
-      while (k < len) {
-        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
-        // b. If SameValueZero(searchElement, elementK) is true, return true.
-        // c. Increase k by 1. 
-        if (sameValueZero(o[k], searchElement)) {
-          return true;
-        }
-        k++;
-      }
-
-      // 8. Return false
-      return false;
-    }
-  });
-}
-
-// Removes:
-// Warning: React depends on requestAnimationFrame. Make sure that you load a
-// polyfill in older browsers.
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback, 0);
-};
+Object.assign = require('object-assign');
