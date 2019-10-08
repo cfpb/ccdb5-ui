4a39d7fa1721a38c66d10fe36625b74e09775b07
Richard Dinh <richard.dinh@cfpb.gov>
2019-10-08 15:36:20 -0400

    update snapshot for tests

---
+++ src/__tests__/TileChartMap.spec.jsx
@@ -0,0 +1,29 @@
+jest.mock('cfpb-chart-builder');
+
+import TileChartMap from '../TileChartMap'
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import thunk from 'redux-thunk'
+
+function setupSnapshot() {
+  const middlewares = [thunk]
+  const mockStore = configureMockStore(middlewares)
+  const store = mockStore({
+  })
+
+  return renderer.create(
+    <Provider store={store}>
+      <TileChartMap />
+    </Provider>
+  )
+}
+
+describe('initial state', () => {
+  it('renders without crashing', () => {
+    const target = setupSnapshot()
+    let tree = target.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
