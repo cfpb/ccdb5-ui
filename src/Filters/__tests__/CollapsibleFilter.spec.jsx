b59d24c0d9d1cefbdd656742f729fec65c648c8c
Richard Dinh <richard.dinh@cfpb.gov>
2019-12-03 11:29:13 -0500

    update tests, update componentDidUpdate

--- src/Filters/__tests__/CollapsibleFilter.spec.jsx
+++ src/Filters/__tests__/CollapsibleFilter.spec.jsx
@@ -3,18 +3,18 @@ import { mount } from 'enzyme';
 import CollapsibleFilter from '../CollapsibleFilter';
 import renderer from 'react-test-renderer';
 
-describe('initial state', () => {
-  it('renders without crashing', () => {
-    const target = renderer.create(
-      <CollapsibleFilter title="foo" desc="bar" />
-    );
-
-    let tree = target.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
-
 describe('component:CollapsibleFilter', () => {
+  describe('initial state', () => {
+    it('renders without crashing', () => {
+      const target = renderer.create(
+        <CollapsibleFilter title="foo" desc="bar" />
+      );
+
+      let tree = target.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
   it('hides the children when Hide is clicked', () => {
     const target = mount(<CollapsibleFilter showChildren={true} />);
     const theButton = target.find('button.o-expandable_cue');
@@ -22,5 +22,18 @@ describe('component:CollapsibleFilter', () => {
     expect(target.state('showChildren')).toEqual(true);
     theButton.simulate('click');
     expect(target.state('showChildren')).toEqual(false);
-  })    
+  })
+
+  describe('componentDidUpdate', () => {
+    it( 'triggers a new state update when props change', () => {
+      const props = {
+        showChildren: false
+      }
+
+      const target = mount( <CollapsibleFilter { ...props } /> )
+      target.setProps( { showChildren: true } )
+      const sv = target.state( 'showChildren' )
+      expect( sv ).toEqual( true )
+    } )
+  })
 })
