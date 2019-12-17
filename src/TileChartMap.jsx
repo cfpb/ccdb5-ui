1f2bdcaa22143d64d7efc2979319e82b7ad82173
Richard Dinh <richard.dinh@cfpb.gov>
2019-12-17 10:14:03 -0500

    tilemap component
    save some work
    
    save work
    
    Revert "save work"
    
    This reverts commit 4ce552871a99707ffdb561f110f9fe806690fca4.
    
    rename file back
    
    update test coverage

---
+++ src/TileChartMap.jsx
@@ -0,0 +1,49 @@
+import './TileChartMap.less'
+import { connect } from 'react-redux'
+import React from 'react'
+import { TileMap } from 'cfpb-chart-builder'
+
+export class TileChartMap extends React.Component {
+  componentDidUpdate( prevProps ) {
+    if(!this.props.data[0].length) {
+      return;
+    }
+
+    const colors = [
+      'rgba(247, 248, 249, 0.5)',
+      'rgba(212, 231, 230, 0.5)',
+      'rgba(180, 210, 209, 0.5)',
+      'rgba(137, 182, 181, 0.5)',
+      'rgba(86, 149, 148, 0.5)',
+      'rgba(37, 116, 115, 0.5)'
+    ];
+
+    const chart = new TileMap( {
+      el: document.getElementById( 'mymap' ),
+      data: this.props.data,
+      colors,
+      localize: true
+    } );
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="mymap"
+             className="cfpb-chart"
+             data-chart-type="tile_map">
+        </div>
+      </div>
+    );
+  }
+
+  // --------------------------------------------------------------------------
+  // Helper methods
+
+  // --------------------------------------------------------------------------
+  // Subrender methods
+}
+
+const mapStateToProps = state => ( { data: [ state.map.state ] } );
+
+export default connect( mapStateToProps )( TileChartMap );
---
+++ src/__tests__/TileChartMap.spec.jsx
@@ -0,0 +1,28 @@
+import TileChartMap from '../TileChartMap'
+import configureMockStore from 'redux-mock-store'
+import { Provider } from 'react-redux'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import thunk from 'redux-thunk'
+
+function setupSnapshot() {
+  const middlewares = [ thunk ]
+  const mockStore = configureMockStore( middlewares )
+  const store = mockStore( {
+    map: {}
+  } );
+
+  return renderer.create(
+    <Provider store={ store }>
+      <TileChartMap/>
+    </Provider>
+  )
+}
+
+describe( 'initial state', () => {
+  it( 'renders without crashing', () => {
+    const target = setupSnapshot()
+    let tree = target.toJSON()
+    expect( tree ).toMatchSnapshot()
+  } )
+} )
