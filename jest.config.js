7d8e0411deca5926fc3e78b2536088b5452707c0
Richard Dinh <richard.dinh@cfpb.gov>
2019-12-17 12:51:03 -0500

    save file transform
    save babel config
    
    updating test coverage
    
    fix for eslint object bug
    
    cherry pick
    
    spacing
    
    update with work from main map and rowchart work
    
    save work
    
    save work

---
+++ jest.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  'collectCoverageFrom': [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+  ],
+  'coveragePathIgnorePatterns': [
+    'src/index.js'
+  ],
+  'resolver': 'jest-pnp-resolver',
+  'setupFiles': [
+    '<rootDir>/config/polyfills.js',
+    '<rootDir>/config/setup.js',
+  ],
+  'testMatch': [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,ts,tsx}',
+  ],
+  'testEnvironment': 'jsdom',
+  'testURL': 'http://localhost',
+  'transform': {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.(css|less)$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  'transformIgnorePatterns': [
+    '^.+\\.module\\.(css|sass|scss)$',
+    'node_modules/(?!(cfpb-chart-builder)/)', // The modules that need to be transpiled. You might not need this.
+    // "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$", // This line is no longer needed.
+  ],
+  'moduleNameMapper': {
+    // "^react-native$": "react-native-web", // This was generated while "ejecting", not necessary.
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '\\.(css|less)$': 'identity-obj-proxy', // If you are importing css/less in JS files
+  },
+  'moduleDirectories': [
+    'node_modules', // This is required
+  ],
+  'moduleFileExtensions': [
+    'web.js',
+    'js',
+    'web.ts',
+    'ts',
+    'web.tsx',
+    'tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+}
