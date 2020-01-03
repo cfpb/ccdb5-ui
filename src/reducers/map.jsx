b3742e0c8348732d1d8371eccdc8e0c277990845
Richard Dinh <richard.dinh@cfpb.gov>
2020-01-03 16:38:17 -0500

    save some work

--- src/reducers/map.jsx
+++ src/reducers/map.jsx
@@ -30,118 +30,6 @@ export const processAggregations = agg => {
   return chartResults
 }
 
-export const processLineChart = inputData => {
-  // tbd fill this in from actual data somewhere
-  if ( inputData ) {
-    return {
-      dataRange: [
-        {
-          count: 2,
-          max: 2487,
-          sum: 4964,
-          avg: 2482,
-          min: 2477,
-          date: '2017-01-01T00:00:00.000Z'
-        },
-        {
-          count: 2,
-          max: 2263,
-          sum: 4300,
-          avg: 2150,
-          min: 2037,
-          date: '2018-01-01T00:00:00.000Z'
-        }
-      ],
-      dataByTopic: [
-        {
-          topicName: 'BANK OF AMERICA, NATIONAL ASSOCIATION',
-          topic: 'BANK OF AMERICA, NATIONAL ASSOCIATION',
-          show: true,
-          readOnly: true,
-          dates: [
-            {
-              date: '2017-01-01T00:00:00.000Z',
-              value: 738
-            },
-            {
-              date: '2018-01-01T00:00:00.000Z',
-              value: 602
-            }
-          ]
-        },
-        {
-          topicName: 'Average of comparison set',
-          topic: 'Average of comparison set',
-          dashed: true,
-          show: true,
-          readOnly: true,
-          dates: [
-            {
-              date: '2017-01-01T00:00:00.000Z',
-              value: 2482
-            },
-            {
-              date: '2018-01-01T00:00:00.000Z',
-              value: 2150
-            }
-          ]
-        },
-        {
-          topicName: 'Sum of comparable companies',
-          topic: 'Sum of comparable companies',
-          show: true,
-          readOnly: true,
-          dates: [
-            {
-              date: '2017-01-01T00:00:00.000Z',
-              value: 4964
-            },
-            {
-              date: '2018-01-01T00:00:00.000Z',
-              value: 4300
-            }
-          ]
-        },
-        {
-          topicName: 'Experian Information Solutions Inc.',
-          topic: 'Experian Information Solutions Inc.',
-          recommended: '',
-          show: false,
-          readOnly: false,
-          dates: [
-            {
-              date: '2017-01-01T00:00:00.000Z',
-              value: 2487
-            },
-            {
-              date: '2018-01-01T00:00:00.000Z',
-              value: 2263
-            }
-          ]
-        },
-        {
-          topicName: 'TRANSUNION INTERMEDIATE HOLDINGS, INC.',
-          topic: 'TRANSUNION INTERMEDIATE HOLDINGS, INC.',
-          recommended: '',
-          show: false,
-          readOnly: false,
-          dates: [
-            {
-              date: '2017-01-01T00:00:00.000Z',
-              value: 2477
-            },
-            {
-              date: '2018-01-01T00:00:00.000Z',
-              value: 2037
-            }
-          ]
-        }
-      ]
-    }
-  }
-
-  return []
-}
 
 export const processStateAggregations = agg => {
   const states = Object.values( agg.state.buckets )
@@ -185,7 +73,7 @@ export default ( state = defaultState, action ) => {
       result.state = processStateAggregations( stateData )
       result.issue = processAggregations( issueData )
       result.product = processAggregations( productData )
-      result.trends = processLineChart(productData)
+
       return result;
     }
 
