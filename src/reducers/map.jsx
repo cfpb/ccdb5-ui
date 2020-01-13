f063ceea513f5256961b8906763540f08906a398
Richard Dinh <richard.dinh@cfpb.gov>
2020-01-13 16:55:41 -0500

    refactor map reducer

--- src/reducers/map.jsx
+++ src/reducers/map.jsx
@@ -42,57 +42,115 @@ export const processStateAggregations = agg => {
       value: o.doc_count,
       issue: o.issue.buckets[0].key,
       product: o.product.buckets[0].key
-    } ) );
+    } ) )
 
-  const stateNames = states.map( o => o.name );
+  const stateNames = states.map( o => o.name )
 
   // patch any missing data
   if ( stateNames.length > 0 ) {
     TILE_MAP_STATES.forEach( o => {
       if ( !stateNames.includes( o ) ) {
-        states.push( { name: o, value: 0, issue: '', product: '' } );
+        states.push( { name: o, value: 0, issue: '', product: '' } )
       }
-    } );
+    } )
   }
   return states
 }
 
-export default ( state = defaultState, action ) => {
-  switch ( action.type ) {
-    case STATES_API_CALLED:
-      return {
-        ...state,
-        activeCall: action.url,
-        isLoading: true
-      }
+// ----------------------------------------------------------------------------
+// Action Handlers
 
-    case STATES_RECEIVED: {
-      const result = { ...state };
-
-      const stateData = action.data.aggregations.state;
-      const issueData = action.data.aggregations.issue;
-      const productData = action.data.aggregations.product;
-      result.activeCall = ''
-      result.isLoading = false
-      result.state = processStateAggregations( stateData )
-      result.issue = processAggregations( issueData )
-      result.product = processAggregations( productData )
-
-      return result;
-    }
-
-    case STATES_FAILED:
-      return {
-        ...state,
-        activeCall: '',
-        issue: [],
-        error: action.error,
-        isLoading: false,
-        product: [],
-        state: []
-      }
-
-    default:
-      return state
+/**
+ * Updates the state when an aggregations call is in progress
+ *
+ * @param {object} state the current state in the Redux store
+ * @param {object} action the payload containing the key/value pairs
+ * @returns {object} the new state for the Redux store
+ */
+export function statesCallInProcess( state, action ) {
+  return {
+    ...state,
+    activeCall: action.url,
+    isLoading: true
   }
+}
+
+/**
+ * Expanded logic for handling aggregations returned from the API
+ *
+ * @param {object} state the current state in the Redux store
+ * @param {object} action the payload containing the key/value pairs
+ * @returns {object} new state for the Redux store
+ */
+export function processStatesResults( state, action ) {
+  const result = { ...state }
+
+  const stateData = action.data.aggregations.state
+  const issueData = action.data.aggregations.issue
+  const productData = action.data.aggregations.product
+  result.activeCall = ''
+  result.isLoading = false
+  result.state = processStateAggregations( stateData )
+  result.issue = processAggregations( issueData )
+  result.product = processAggregations( productData )
+
+  return result
+}
+
+/**
+ * handling errors from an aggregation call
+ *
+ * @param {object} state the current state in the Redux store
+ * @param {object} action the payload containing the key/value pairs
+ * @returns {object} new state for the Redux store
+ */
+export function processStatesError( state, action ) {
+  return {
+    ...state,
+    activeCall: '',
+    issue: [],
+    error: action.error,
+    isLoading: false,
+    product: [],
+    state: []
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Action Handlers
+
+/**
+ * Creates a hash table of action types to handlers
+ *
+ * @returns {object} a map of types to functions
+ */
+export function _buildHandlerMap() {
+  const handlers = {}
+  handlers[STATES_API_CALLED] = statesCallInProcess
+  handlers[STATES_RECEIVED] = processStatesResults
+  handlers[STATES_FAILED] = processStatesError
+
+  return handlers
+}
+
+const _handlers = _buildHandlerMap()
+
+/**
+ * Routes an action to an appropriate handler
+ *
+ * @param {object} state the current state in the Redux store
+ * @param {object} action the command being executed
+ * @returns {object} the new state for the Redux store
+ */
+function handleSpecificAction( state, action ) {
+  if ( action.type in _handlers ) {
+    return _handlers[action.type]( state, action )
+  }
+
+  return state
+}
+
+export default ( state = defaultState, action ) => {
+  const newState = handleSpecificAction( state, action )
+  return newState
 }
