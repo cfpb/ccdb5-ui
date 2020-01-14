880ca3e8c3c65f548c9d8872e11472fa2ee04cd6
Richard Dinh <richard.dinh@cfpb.gov>
2020-01-14 15:42:14 -0500

    add map toolbar to map tab

---
+++ src/MapToolbar.jsx
@@ -0,0 +1,50 @@
+import './MapToolbar.less'
+import { connect } from 'react-redux'
+import React from 'react'
+import { tabChanged } from './actions/view'
+
+export class MapToolbar extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      tab: props.tab
+    };
+  }
+
+  _setTab( tab ) {
+    window.scrollTo( 0, 0 )
+    this.props.onTab( tab );
+  }
+
+
+  render() {
+    return (
+      <div className="mapToolbar">
+        <section>
+          <button className="map"
+                  onClick={() => this._setTab( 'Map' )}>
+            Map
+          </button>
+
+          <button className="list"
+                  onClick={() => this._setTab( 'List' )}>
+            List
+          </button>
+        </section>
+      </div>
+    );
+  }
+}
+
+export const mapStateToProps = state => ( {
+  tab: state.query.tab
+} )
+
+export const mapDispatchToProps = dispatch => ( {
+  onTab: tab => {
+    dispatch( tabChanged( tab ) )
+  }
+} );
+
+export default connect( mapStateToProps,
+  mapDispatchToProps )( MapToolbar )
