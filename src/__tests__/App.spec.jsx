81e8f9b7cd0904d15dcd05d21e0db58f88eb5a5e
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2020-02-26 16:13:54 -0500

    test: Address Jest warnings
    Silence the warning from here: https://fb.me/react-wrap-tests-with-act

--- src/__tests__/App.spec.jsx
+++ src/__tests__/App.spec.jsx
@@ -1,3 +1,4 @@
+import { act, create } from 'react-test-renderer';
 import { App, DetailComponents } from '../App';
 import accessibility from 'highcharts/modules/accessibility';
 import configureMockStore from 'redux-mock-store'
@@ -6,14 +7,13 @@ import Highcharts from 'highcharts/highmaps';
 import { MemoryRouter } from 'react-router';
 import { Provider } from 'react-redux'
 import React from 'react';
-import renderer from 'react-test-renderer';
 import thunk from 'redux-thunk'
 
 jest.mock( 'highcharts/modules/accessibility' )
 jest.mock( 'highcharts/highmaps' )
 
 describe('initial state', () => {
-  it('renders without crashing', () => {
+  it('renders without crashing', async () => {
 
     // set the date so snapshot will always be the same.
     const DATE_TO_USE = new Date('2016');
@@ -24,11 +24,14 @@ describe('initial state', () => {
     global.Date.now = _Date.now;
     defaultQuery.searchText = 'foo';
 
-    const target = renderer.create(
-      <MemoryRouter initialEntries={[ '/' ]}>
-        <App />
-      </MemoryRouter>
-    );
+    let target
+    await act( async () => {
+      target = create(
+        <MemoryRouter initialEntries={[ '/' ]}>
+          <App />
+        </MemoryRouter>
+      )
+    } )
 
     let tree = target.toJSON();
     expect(tree).toMatchSnapshot();
@@ -43,7 +46,7 @@ describe('initial state', () => {
     })
 
     const match = { params: { id: '1234' } };
-    const detailTarget = renderer.create(
+    const detailTarget = create(
       <MemoryRouter initialEntries={[ '/detail/1234' ]}>
         <Provider store={store}>
           <DetailComponents match={ match }/>
