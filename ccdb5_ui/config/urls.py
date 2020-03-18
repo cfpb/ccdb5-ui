b66ecd419c0d62cdbbb4e055d3a7ee27dc5aa289
Craig Davies <craig.davies@cfpb.gov>
2020-03-18 14:48:11 -0400

    Support Django 2.2

--- ccdb5_ui/config/urls.py
+++ ccdb5_ui/config/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import url
 from ccdb5_ui.views import CCDB5MainView
 
+
+try:
+    from django.urls import re_path
+except ImportError:
+    from django.conf.urls import url as re_path
+
+
 urlpatterns = [
-    url(r'^.*$', CCDB5MainView.as_view()),
+    re_path(r'^.*$', CCDB5MainView.as_view()),
 ]
