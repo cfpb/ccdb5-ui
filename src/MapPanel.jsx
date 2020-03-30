ecaa57ba9d1fa095ec08ca7f51690d528f9ae130
Richard Dinh <richard.dinh@cfpb.gov>
2020-03-30 16:45:50 -0400

    update view reducer, adding more logic, added a mobile filter

--- src/MapPanel.jsx
+++ src/MapPanel.jsx
@@ -3,6 +3,7 @@ import ActionBar from './ActionBar'
 import { connect } from 'react-redux'
 import DateIntervals from './DateIntervals'
 import ErrorBlock from './Error'
+import FilterPanel from './FilterPanel'
 import { hasFiltersEnabled } from './utils'
 import Loading from './Dialogs/Loading'
 import MapToolbar from './MapToolbar'
@@ -12,6 +13,7 @@ import RowChart from './RowChart'
 import TileChartMap from './TileChartMap'
 import Warning from './Warning'
 
+
 const WARNING_MESSAGE = 'Due to your filter selections, the “Complaints per' +
   ' 1,000” option has been disabled'
 
@@ -20,6 +22,7 @@ export class MapPanel extends React.Component {
     return (
       <section className="map-panel">
         <ActionBar/>
+        { this.props.showMobileFilters && <FilterPanel/> }
         { this.props.error &&
           <ErrorBlock text="There was a problem executing your search" />
         }
@@ -41,6 +44,7 @@ export class MapPanel extends React.Component {
 const mapStateToProps = state => ( {
   error: state.map.error,
   isLoading: state.map.isLoading,
+  showMobileFilters: state.view.width < 749,
   showWarning: hasFiltersEnabled( state.query )
 } )
 
