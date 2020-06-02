7554343cb777c6e1ed37b7fd44153ea37f841f15
Richard Dinh <richard.dinh@cfpb.gov>
2020-06-02 08:47:55 -0400

    update test coverage for lenstabs

--- src/components/__tests__/LensTabs.spec.jsx
+++ src/components/__tests__/LensTabs.spec.jsx
@@ -1,13 +1,18 @@
 import configureMockStore from 'redux-mock-store'
 import { IntlProvider } from 'react-intl'
 import { Provider } from 'react-redux'
-import { LensTabs, mapDispatchToProps } from '../Trends/LensTabs'
-import { MODE_MAP, REQUERY_ALWAYS } from '../../constants'
+import {
+  LensTabs,
+  mapDispatchToProps,
+  mapStateToProps
+} from '../Trends/LensTabs'
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { REQUERY_ALWAYS } from '../../constants'
 import thunk from 'redux-thunk'
+import { shallow } from 'enzyme'
 
-function setupSnapshot( printMode ) {
+function setupSnapshot() {
   const middlewares = [ thunk ]
   const mockStore = configureMockStore( middlewares )
   const store = mockStore( {} )
@@ -28,6 +33,30 @@ describe( 'component:LensTabs', () => {
     expect( tree ).toMatchSnapshot()
   } )
 
+  describe( 'buttons', () => {
+    let cb = null
+    let target = null
+
+    beforeEach( () => {
+      cb = jest.fn()
+      target = shallow( <LensTabs onTab={ cb }
+                                  lens={ 'Product' }
+                                  subLens={ 'Issue' }/> )
+    } )
+
+    it( 'tabChanged is called with Product when the button is clicked', () => {
+      const prev = target.find( '.tabbed-navigation button.sub_product' )
+      prev.simulate( 'click' )
+      expect( cb ).toHaveBeenCalledWith( 'sub_Product' )
+    } )
+
+    it( 'tabChanged is called with Issue when the button is clicked', () => {
+      const prev = target.find( '.tabbed-navigation button.issue' )
+      prev.simulate( 'click' )
+      expect( cb ).toHaveBeenCalledWith( 'Issue' )
+    } )
+  } )
+
   describe( 'mapDispatchToProps', () => {
     it( 'hooks into changeDataSubLens', () => {
       const dispatch = jest.fn()
@@ -40,5 +69,19 @@ describe( 'component:LensTabs', () => {
         } ]
       ] )
     } )
-  })
+  } )
+
+  describe( 'mapStateToProps', () => {
+    it( 'maps state and props', () => {
+      const state = {
+        query: {
+          lens: 'foo',
+          subLens: 'bar'
+        }
+      }
+      let actual = mapStateToProps( state )
+      expect( actual ).toEqual( { lens: 'foo', subLens: 'bar' } )
+    } )
+  } )
+
 } )
