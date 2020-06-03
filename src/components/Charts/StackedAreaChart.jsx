4bed776eb4523b280503ed81508adc2919e96475
Richard Dinh <richard.dinh@cfpb.gov>
2020-06-03 11:58:24 -0400

    Line Chart updates
    save some work
    
    update a test
    
    error handling
    
    stacked area chart
    
    snapshot
    
    fixing bug with tooltip
    
    eslint nag

---
+++ src/components/Charts/StackedAreaChart.jsx
@@ -0,0 +1,120 @@
+import './StackedAreaChart.less'
+import * as d3 from 'd3'
+import { connect } from 'react-redux'
+import { getLastDate } from '../../utils/chart'
+import { hashObject } from '../../utils'
+import { isDateEqual } from '../../utils/formatDate'
+import PropTypes from 'prop-types'
+import React from 'react'
+import { stackedArea } from 'britecharts'
+import { updateTrendsTooltip } from '../../actions/trends'
+
+
+export class StackedAreaChart extends React.Component {
+  constructor( props ) {
+    super( props )
+    this._updateTooltip = this._updateTooltip.bind( this )
+  }
+
+  componentDidMount() {
+    this._redrawChart()
+  }
+
+  componentDidUpdate( prevProps ) {
+    const props = this.props
+    if ( hashObject( prevProps.data ) !== hashObject( props.data ) ||
+      hashObject( prevProps.width ) !== hashObject( props.width ) ||
+      hashObject( prevProps.printMode ) !== hashObject( props.printMode ) ) {
+      this._redrawChart()
+    }
+  }
+
+  _updateTooltip( point ) {
+    if ( !isDateEqual( this.props.tooltip.date, point.date ) ) {
+      this.props.tooltipUpdated( {
+        date: point.date,
+        dateRange: this.props.dateRange,
+        interval: this.props.interval,
+        values: point.values
+      } )
+    }
+  }
+
+  _redrawChart() {
+    const { colorMap, data, dateRange, interval, lastDate } = this.props
+    if ( !data || !data.length ) {
+      return
+    }
+
+    const chartID = '#stacked-area-chart'
+    const container = d3.select( chartID )
+    const containerWidth = container.node().getBoundingClientRect().width
+    d3.select( chartID + ' .stacked-area' ).remove()
+    const stackedAreaChart = stackedArea()
+    const colors = Object.values( colorMap )
+
+    stackedAreaChart.margin( { left: 50, right: 10, top: 10, bottom: 40 } )
+      .areaCurve( 'linear' )
+      .initializeVerticalMarker( true )
+      .isAnimated( false )
+      .tooltipThreshold( 1 )
+      .grid( 'horizontal' )
+      .aspectRatio( 0.5 )
+      .width( containerWidth )
+      .dateLabel( 'date' )
+      .colorSchema( colors )
+      .on( 'customMouseMove', this._updateTooltip )
+
+    container.datum( data ).call( stackedAreaChart )
+
+    const config = {
+      dateRange,
+      interval,
+      lastDate
+    }
+
+    this.props.tooltipUpdated( getLastDate( data, config ) )
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{ this.props.title }</h2>
+        <div id="stacked-area-chart">
+        </div>
+      </div>
+    )
+  }
+}
+
+export const mapDispatchToProps = dispatch => ( {
+  tooltipUpdated: selectedState => {
+    // Analytics.sendEvent(
+    //   Analytics.getDataLayerOptions( 'Trend Event: add',
+    //     selectedState.abbr, )
+    // )
+    dispatch( updateTrendsTooltip( selectedState ) )
+  }
+} )
+
+export const mapStateToProps = state => ( {
+  colorMap: state.trends.colorMap,
+  data: state.trends.results.dateRangeArea,
+  dateRange:  {
+    from: state.query.date_received_min,
+    to: state.query.date_received_max
+  },
+  interval: state.query.dateInterval,
+  lastDate: state.trends.lastDate,
+  lens: state.trends.lens,
+  printMode: state.view.printMode,
+  tooltip: state.trends.tooltip,
+  width: state.view.width
+} )
+
+StackedAreaChart.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+export default connect( mapStateToProps,
+  mapDispatchToProps )( StackedAreaChart )
--- src/utils/chart.jsx
+++ src/utils/chart.jsx
@@ -1,18 +1,62 @@
 // ----------------------------------------------------------------------------
 /* eslint-disable no-mixed-operators */
 import { clampDate, slugify } from '../utils'
-import {
-  formatDateView
-} from './formatDate'
+import { formatDateView, isDateEqual } from './formatDate'
 
 import moment from 'moment'
+
+export const getLastDate = ( dataSet, config ) => {
+  // take in array of data points
+  // early exit
+  if ( !dataSet || dataSet.length === 0 ) {
+    return null
+  }
+
+  const lastDate = config.lastDate
+  const lastPointValues = dataSet.filter( o => isDateEqual( o.date, lastDate ) )
+  return {
+    key: lastDate,
+    date: lastDate,
+    dateRange: config.dateRange,
+    interval: config.interval,
+    values: lastPointValues
+  }
+}
+
+export const getLastLineDate = ( dataSet, config ) => {
+  // take in array of data points
+  if ( !dataSet || !dataSet.dataByTopic || dataSet.dataByTopic.length === 0 ) {
+    return null
+  }
+
+  const lastDate = config.lastDate
+  const values = dataSet.dataByTopic.map( o => {
+    const lastPoint = o.dates.find( v => isDateEqual( v.date, lastDate ) )
+    const value = lastPoint ? lastPoint.value : 0
+    return {
+      name: o.topic,
+      date: lastDate,
+      value
+    }
+  } )
+
+  const lastPoint = {
+    key: lastDate,
+    date: lastDate,
+    dateRange: config.dateRange,
+    interval: config.interval,
+    values
+  }
+  return lastPoint
+}
+
 
 export const getTooltipDate = ( inputDate, dateRange ) => {
   const returnDate = clampDate( inputDate, dateRange.from, dateRange.to )
   return formatDateView( returnDate )
 }
 
-export const getTooltipTitle = ( inputDate, interval, dateRange ) => {
+export const getTooltipTitle = ( inputDate, interval, dateRange, external ) => {
   /* eslint complexity: ["error", 6] */
   interval = interval.toLowerCase()
   const startDate = getTooltipDate( inputDate, dateRange )
@@ -40,7 +84,12 @@ export const getTooltipTitle = ( inputDate, interval, dateRange ) => {
 
   endDate = getTooltipDate( endDate, dateRange )
 
-  return interval === 'day' ? endDate : `${ startDate } - ${ endDate }`
+  if ( interval === 'day' ) {
+    return `Date: ${ endDate }`
+  }
+
+  return external ? `Date Interval: ${ startDate } - ${ endDate }` :
+    `${ startDate } - ${ endDate }`
 }
 
 /**
@@ -68,9 +117,9 @@ export const getColorScheme = ( rowNames, colorMap ) =>
   } )
 
 export const processRows = ( filters, rows, colorMap ) => {
-  let data = rows
+  let data = rows ? rows : []
   // only include the bars that are explicitly listed in the filters
-  if ( filters && filters.length ) {
+  if ( filters && filters.length && data ) {
     data = data.filter( o => {
       if ( o.parent ) {
         return filters.includes( slugify( o.parent, o.name ) )
@@ -81,11 +130,10 @@ export const processRows = ( filters, rows, colorMap ) => {
   }
 
   data = data.filter( o => o.visible )
-
   const colorScheme = getColorScheme( data, colorMap )
 
   return {
     colorScheme,
-    data: data.filter( o => o.visible )
+    data
   }
 }
