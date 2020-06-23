be187e73aed2efb1d863487b30d9de549573642f
Richard Dinh <richard.dinh@cfpb.gov>
2020-06-23 11:52:25 -0400

    print fixes

--- src/reducers/query.jsx
+++ src/reducers/query.jsx
@@ -12,6 +12,7 @@ const queryString = require( 'query-string' );
 
 /* eslint-disable camelcase */
 export const defaultQuery = {
+  chartType: 'line',
   dateInterval: 'Month',
   dateRange: '3y',
   date_received_max: startOfToday(),
@@ -48,7 +49,7 @@ const trendFieldMap = {
 
 const urlParams = [
   'dateRange', 'searchText', 'searchField', 'tab',
-  'lens', 'dateInterval', 'subLens', 'focus'
+  'lens', 'dateInterval', 'subLens', 'focus', 'chartType'
 ]
 const urlParamsInt = [ 'from', 'page', 'size' ]
 
@@ -750,6 +751,20 @@ function changeDataSubLens( state, action ) {
   }
 }
 
+/**
+ * Handler for the update chart type action
+ *
+ * @param {object} state the current state in the Redux store
+ * @param {object} action the command being executed
+ * @returns {object} the new state for the Redux store
+ */
+export function updateChartType( state, action ) {
+  return {
+    ...state,
+    chartType: action.chartType
+  }
+}
+
 // ----------------------------------------------------------------------------
 // Query String Builder
 
@@ -828,6 +843,7 @@ export function validatePer1000( queryState ) {
 export function _buildHandlerMap() {
   const handlers = {}
   handlers[actions.FOCUS_CHANGED] = changeFocus
+  handlers[actions.CHART_TYPE_CHANGED] = updateChartType
   handlers[actions.COMPLAINTS_RECEIVED] = updateTotalPages
   handlers[actions.DATA_LENS_CHANGED] = changeDataLens
   handlers[actions.DATA_SUBLENS_CHANGED] = changeDataSubLens
