ef1c6b0616299ffbba0e2ec6edd125e59e3960ec
Richard Dinh <richard.dinh@cfpb.gov>
2021-08-24 16:46:42 -0400

    put back stuff

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,3 @@
-
-// force this so that our tooltip tests and calculations are standardized
-
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
