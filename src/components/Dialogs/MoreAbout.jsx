3d54c9135560ce57bd6f90c6d771ca553686284c
Ans <git@anselmbradford.com>
2022-02-15 13:57:56 -0500

    Update modal text

--- src/components/Dialogs/MoreAbout.jsx
+++ src/components/Dialogs/MoreAbout.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default class MoreAbout extends React.Component {
   render() {
-    const urlScrubbing = 'https://files.consumerfinance.gov/a/assets/' +
-      '201503_cfpb_Narrative-Scrubbing-Standard.pdf'
+    const urlScrubbing = 'https://files.consumerfinance.gov/f/documents/' +
+      '201503_cfpb_disclosure-of-consumer-complaint-narrative-data.pdf'
 
     const urlCriteria = 'https://files.consumerfinance.gov/f/' +
       '201303_cfpb_Final-Policy-Statement-Disclosure-of-Consumer-Complaint-' +
@@ -11,37 +11,49 @@ export default class MoreAbout extends React.Component {
 
     return (
       <section className="more-about-modal">
-        <div className="header layout-row">
-          <h3 className="flex-all">More about the complaint database</h3>
+        <div className="header">
+          <h3>
+            Things you should know before you use this database
+          </h3>
         </div>
         <div className="body">
           <p className="body-copy">
-            We don’t verify all the facts alleged in these complaints, but we
-            do give companies the opportunity to confirm they have a commercial
-            relationship with the consumer before we publish the complaint in
-            this database.
+            This database is not a statistical sample of consumers’ experiences
+            in the marketplace.
+            Complaints are not necessarily representative of all consumers’
+            experiences with a financial product or company.
           </p>
           <p className="body-copy">
-            We only publish the description of what happened if the consumer
-            agrees to share it and after we take steps to&nbsp;
+            The database only includes complaints the CFPB sent to companies and
+            complaints are only published after the company responds, confirming
+            a commercial relationship or after 15 days, whichever comes first.
+            The database does not include complaints referred to other
+            regulators, such as complaints about depository institutions with
+            less than $10 billion in assets. Company-specific information should
+            be considered in the context of that company’s size and/or market
+            share.
+          </p>
+          <p className="body-copy">
+            We only publish a consumer’s description of what happened in their
+            own words if the consumer agrees to share it and after we take steps
+            to&nbsp;
             <a href={urlScrubbing}
                target="_blank"
                rel="noopener noreferrer">
-              remove personal information.
-            </a>
+              remove personal information
+            </a>.&nbsp;
+            One consumer’s experience is not necessarily representative of all
+            consumers’ experiences and narratives are not verified before
+            publication.
           </p>
           <p className="body-copy">
-            We don’t publish complaints if they don't meet our&nbsp;
+            The database generally updates daily. We don’t publish complaints if
+            they don’t meet our&nbsp;
             <a href={urlCriteria}
                target="_blank"
                rel="noopener noreferrer">
-              publication criteria.
-            </a>
-            &nbsp;Data is generally updated daily.
-          </p>
-          <p className="body-copy">
-            Company specific information should be considered in the context of
-            that company's size and/or market share.
+              publication criteria
+            </a>.
           </p>
         </div>
         <div className="footer layout-row">
