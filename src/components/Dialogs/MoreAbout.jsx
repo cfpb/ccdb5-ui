60f171c5c8b8051a04d31c88fd4cbbd57977e7bb
Ans <git@anselmbradford.com>
2022-02-15 16:08:04 -0500

    Update modal copy

--- src/components/Dialogs/MoreAbout.jsx
+++ src/components/Dialogs/MoreAbout.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 export default class MoreAbout extends React.Component {
   render() {
     const urlScrubbing = 'https://files.consumerfinance.gov/f/documents/' +
-      '201503_cfpb_disclosure-of-consumer-complaint-narrative-data.pdf'
+      '201503_cfpb_Narrative-Scrubbing-Standard.pdf'
 
-    const urlCriteria = 'https://files.consumerfinance.gov/f/' +
-      '201303_cfpb_Final-Policy-Statement-Disclosure-of-Consumer-Complaint-' +
-      'Data.pdf'
+    const urlCriteria = 'https://files.consumerfinance.gov/f/documents/' +
+      '201503_cfpb_disclosure-of-consumer-complaint-narrative-data.pdf'
 
     return (
       <section className="more-about-modal">
@@ -18,25 +17,24 @@ export default class MoreAbout extends React.Component {
         </div>
         <div className="body">
           <p className="body-copy">
+            This database only includes complaints the CFPB sent to companies
+            and complaints are only published after the company responds,
+            confirming a commercial relationship or after 15 days, whichever
+            comes first. This database does not include complaints referred to
+            other regulators, such as complaints about depository institutions
+            with less than $10 billion in assets.
+          </p>
+          <p className="body-copy">
             This database is not a statistical sample of consumers’ experiences
-            in the marketplace.
-            Complaints are not necessarily representative of all consumers’
-            experiences with a financial product or company.
+            in the marketplace. Complaints are not necessarily representative
+            of all consumers’ experiences with a financial product or company.
+            Company-specific information should be considered in the context of
+            that company’s size and/or market share.
           </p>
           <p className="body-copy">
-            The database only includes complaints the CFPB sent to companies and
-            complaints are only published after the company responds, confirming
-            a commercial relationship or after 15 days, whichever comes first.
-            The database does not include complaints referred to other
-            regulators, such as complaints about depository institutions with
-            less than $10 billion in assets. Company-specific information should
-            be considered in the context of that company’s size and/or market
-            share.
-          </p>
-          <p className="body-copy">
-            We only publish a consumer’s description of what happened in their
-            own words if the consumer agrees to share it and after we take steps
-            to&nbsp;
+            This database only includes a consumer’s description of what
+            happened in their own words if the consumer agrees to share it and
+            after we take steps to&nbsp;
             <a href={urlScrubbing}
                target="_blank"
                rel="noopener noreferrer">
@@ -47,8 +45,8 @@ export default class MoreAbout extends React.Component {
             publication.
           </p>
           <p className="body-copy">
-            The database generally updates daily. We don’t publish complaints if
-            they don’t meet our&nbsp;
+            This database generally updates daily and doesn’t include complaints
+            that don’t meet our&nbsp;
             <a href={urlCriteria}
                target="_blank"
                rel="noopener noreferrer">
