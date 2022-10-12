d73f2590ecb525a03e60c3fe871ff0207663741a
Richard Dinh <richard.dinh@cfpb.gov>
2022-10-12 09:37:11 -0400

    convert hero to functional component
    save ref
    
    update hero to functional, update snapshots
    
    up bundle

---
+++ src/components/Search/Hero/Hero.js
@@ -0,0 +1,41 @@
+import './Hero.less';
+import { useDispatch } from 'react-redux';
+import React from 'react';
+import { LINK_DATA_USE } from '../../../constants';
+import { showMoreAboutDialog } from '../../../actions/view';
+
+export const Hero = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <header className="content_hero">
+      <h1 className="content-header">Consumer Complaint Database</h1>
+      <ul className="m-list m-list__horizontal">
+        <li className="m-list_item">
+          <button
+            className="a-btn a-btn__link"
+            onClick={() => {
+              dispatch(showMoreAboutDialog());
+            }}
+          >
+            Things to know before you use this database
+          </button>
+        </li>
+        <li className="m-list_item">
+          <a href={LINK_DATA_USE} target="_blank" rel="noopener noreferrer">
+            How we use complaint data
+          </a>
+        </li>
+        <li className="m-list_item">
+          <a
+            href="https://cfpb.github.io/api/ccdb/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Technical documentation
+          </a>
+        </li>
+      </ul>
+    </header>
+  );
+};
