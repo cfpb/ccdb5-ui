2c05607396b78f695ab8e5bd76527657c9ecf7be
Richard Dinh <richard.dinh@cfpb.gov>
2022-10-14 11:59:19 -0400

    functional component, moved test, adding selector (#442)
    update test, remove snapshot
    
    actionbar.less doesnt need select.less file
    
    lint

--- src/reducers/aggs/selectors.js
+++ src/reducers/aggs/selectors.js
@@ -1,2 +1,4 @@
 export const selectAggsState = (state) => state.aggs;
+export const selectAggsDocCount = (state) => state.aggs.doc_count;
 export const selectAggsHasError = (state) => state.aggs.error;
+export const selectAggsTotal = (state) => state.aggs.total;
