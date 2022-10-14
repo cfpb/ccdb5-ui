e94f8a5c2cdbb30d7629d28509de4be519baffdf
KatherynRizek <katheryn.rizek@cfpb.gov>
2022-10-14 12:44:16 -0400

    ZipCode mostly working

--- src/components/Filters/ZipCode.js
+++ src/components/Filters/ZipCode.js
@@ -1,56 +1,39 @@
 import React, { useState } from 'react';
-// import { AsyncTypeahead } from 'react-bootstrap-typeahead';
-// import { coalesce } from '../../utils';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import { addMultipleFilters } from '../../actions/filter';
 import CollapsibleFilter from './CollapsibleFilter';
-import { useDispatch, useSelector } from 'react-redux';
-// import HighlightingOption from '../Typeahead/HighlightingOption';
-// import PropTypes from 'prop-types';
 import { stateToQS } from '../../reducers/query/query';
-// import StickyOptions from './StickyOptions';
-// import Typeahead from '../Typeahead';
 import { API_PLACEHOLDER } from '../../constants';
-// import { selectAggsState } from '../../reducers/aggs/selectors';
 import { selectQueryState } from '../../reducers/query/selectors';
 import { AsyncTypeahead } from '../Typeahead/AsyncTypeahead';
 
 const FIELD_NAME = 'zip_code';
 
-export const ZipCode = () => {
+export const ZipCode = ({ delayWait }) => {
   const dispatch = useDispatch();
-  // const typeaheadRef = useRef();
-  // const aggs = useSelector(selectAggsState);
   const query = useSelector(selectQueryState);
   const [dropdownOptions, setDropdownOptions] = useState([]);
 
-  // const options = coalesce(aggs, FIELD_NAME, []);
   const queryState = Object.assign({}, query);
-  // make sure searchAfter doesn't appear, it'll mess up your search endpoint
   queryState.searchAfter = '';
   const queryString = stateToQS(queryState);
-  // const selections = coalesce(state.query, FIELD_NAME, []);
 
-  const typeaheadSelect = (value) => {
-    dispatch(addMultipleFilters(FIELD_NAME, [value]));
+  const onSelection = (value) => {
+    dispatch(addMultipleFilters(FIELD_NAME, [value[0].key]));
     setDropdownOptions([]);
-    // typeaheadRef.current.clear();
   };
 
-  const _onInputChange = (value) => {
+  const onInputChange = (value) => {
     const n = value.toLowerCase();
-    // typeaheadRef.current.setState(value);
-
     if (n === '') {
       setDropdownOptions([]);
       return;
     }
-
     const qs = queryString + '&text=' + value;
-
     const uri = `${API_PLACEHOLDER}_suggest_zip/${qs}`;
     fetch(uri)
       .then((response) => response.json())
-      // .then((data) => setDropdownOptions(data));
       .then((items) => {
         const options = items.map((x) => ({
           key: x,
@@ -58,20 +41,10 @@ export const ZipCode = () => {
           position: x.toLowerCase().indexOf(n),
           value,
         }));
-        console.log(options);
         setDropdownOptions(options);
       });
-    // setDropdownOptions(results);
   };
 
-  // const _renderOptions = (obj) => {
-  //   return <HighlightingOption {...obj} />;
-  // };
-
-  const _onOptionSelected = (item) => {
-    console.log(item);
-    typeaheadSelect(item[0].key);
-  };
   return (
     <CollapsibleFilter
       title="ZIP code"
@@ -79,54 +52,22 @@ export const ZipCode = () => {
       className="aggregation"
     >
       <AsyncTypeahead
-        // className="typeahead"
         htmlId="zipcode-typeahead"
         ariaLabel="Start typing to begin listing zip codes"
-        // filterBy={filterBy}
-        // ref={ref}
-        // newSelectionPrefix="Custom search: "
-        // onChange={(selected) => handleChange(selected)}
-        // onKeyDown={(event) => handlePressEnter(event)}
-        handleSearch={_onInputChange}
-        handleChange={_onOptionSelected}
+        delayWait={delayWait}
+        handleSearch={onInputChange}
+        handleChange={onSelection}
         options={dropdownOptions}
         placeholder="Enter first three digits of ZIP code"
-        // paginate={false}
-        // placeholder="Enter first three digits of ZIP code"
-        // renderMenuItemChildren={_renderOptions}
-        // ref={typeaheadRef}
       />
-      {/* <AsyncTypeahead
-        id="zipcode-typeahead"
-        minLength={2}
-        className="typeahead"
-        isLoading={false}
-        // filterBy={filterBy}
-        // ref={ref}
-        // newSelectionPrefix="Custom search: "
-        // onChange={(selected) => handleChange(selected)}
-        // onKeyDown={(event) => handlePressEnter(event)}
-        onSearch={_onInputChange}
-        onChange={_onOptionSelected}
-        options={dropdownOptions}
-        maxResults={5}
-        // paginate={false}
-        placeholder="Enter first three digits of ZIP code"
-        renderMenuItemChildren={_renderOptions}
-        data-cy="input-search"
-      /> */}
     </CollapsibleFilter>
   );
 };
 
-// ZipCode.propTypes = {
-//   // debounceWait: PropTypes.number,
-//   options: PropTypes.array.isRequired,
-//   selections: PropTypes.array,
-//   queryString: PropTypes.string.isRequired,
-//   typeaheadSelect: PropTypes.func.isRequired,
-// };
+ZipCode.propTypes = {
+  delayWait: PropTypes.number,
+};
 
-// ZipCode.defaultProps = {
-//   debounceWait: 250,
-// };
+ZipCode.defaultProps = {
+  delayWait: 250,
+};
--- src/components/Typeahead/AsyncTypeahead.js
+++ src/components/Typeahead/AsyncTypeahead.js
@@ -1,29 +1,27 @@
-import 'react-bootstrap-typeahead/css/Typeahead.css';
+// import 'react-bootstrap-typeahead/css/Typeahead.css';
 import './Typeahead.less';
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { AsyncTypeahead as Typeahead } from 'react-bootstrap-typeahead';
 import iconMap from '../iconMap';
 import HighlightingOption from './HighlightingOption';
-// import HighlightingOption from './HighlightingOption';
 
 export const AsyncTypeahead = ({
   className,
+  delayWait,
   htmlId,
   ariaLabel,
   handleChange,
   handleSearch,
+  maxResults,
+  minLength,
   options,
   placeholder,
-  //   ref,
 }) => {
   const ref = useRef();
+
   return (
-    <section
-      className={`typeahead ${className}`}
-      // onBlur={this._onBlur}
-      // onFocus={this._onFocus}
-    >
+    <section className={`typeahead ${className | ''}`}>
       <div className="m-btn-inside-input input-contains-label">
         <div
           className="input-contains-label_before
@@ -36,69 +34,49 @@ export const AsyncTypeahead = ({
         </label>
         <Typeahead
           id="zipcode-typeahead"
-          minLength={2}
+          minLength={minLength}
           className="typeahead-selector"
-          //   clearButton={true}
+          clearButton={true}
+          delay={delayWait}
           isLoading={false}
-          // filterBy={filterBy}
-          // ref={ref}
-          // newSelectionPrefix="Custom search: "
-          // onChange={(selected) => handleChange(selected)}
-          // onKeyDown={(event) => handlePressEnter(event)}
+          ref={ref}
           onSearch={(input) => handleSearch(input)}
           onChange={(selected) => {
             handleChange(selected);
             ref.current.clear();
           }}
           options={options}
-          maxResults={5}
-          // paginate={false}
+          maxResults={maxResults}
           placeholder={placeholder}
-          //   renderMenuItemChildren={_renderOptions}
           renderMenuItemChildren={(option) => (
             <li className="typeahead-option body-copy">
               <HighlightingOption {...option} />
             </li>
           )}
-          ref={ref}
           data-cy="input-search"
         />
-        {/* <input
-            type="text"
-            autoComplete="off"
-            className="a-text-input"
-            disabled={this.props.isDisabled}
-            id={this.props.htmlId}
-            onChange={this._valueUpdated}
-            onKeyDown={this._onKeyDown}
-            placeholder={this.props.placeholder}
-            value={this.state.inputValue}
-            {...this.props.textBoxProps}
-          />
-          {this.state.inputValue ? clear : null} */}
       </div>
-      {/* {this.state.focused ? this.renderMap[this.state.phase]() : null} */}
     </section>
   );
 };
 
 AsyncTypeahead.propTypes = {
-  // debounceWait: PropTypes.number,
-  ariaLabel: PropTypes.string,
+  ariaLabel: PropTypes.string.isRequired,
   className: PropTypes.string,
-  //   debounceWait: PropTypes.number,
-  //   isDisabled: PropTypes.bool,
-  //   isDisableTypeahead: PropTypes.bool,
-  htmlId: PropTypes.string,
-  //   maxVisible: PropTypes.number,
-  //   minLength: PropTypes.number,
-  //   mode: PropTypes.oneOf([MODE_OPEN, MODE_CLOSED]).isRequired,
-  handleChange: PropTypes.func,
-  handleSearch: PropTypes.func,
-  placeholder: PropTypes.string,
-  //   renderOption: PropTypes.func,
-  ref: PropTypes.element,
-  //   textBoxProps: PropTypes.object,
-  //   value: PropTypes.string,
+  delayWait: PropTypes.number.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  htmlId: PropTypes.string.isRequired,
+  maxResults: PropTypes.number,
+  minLength: PropTypes.number,
   options: PropTypes.array,
+  placeholder: PropTypes.string,
+};
+
+AsyncTypeahead.defaultProps = {
+  className: '',
+  delayWait: 0,
+  maxResults: 5,
+  minLength: 2,
+  placeholder: 'Enter your search text',
 };
