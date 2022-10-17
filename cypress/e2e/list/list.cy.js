312be94d28d169d082753c84ae3bf4eb01ae404c
Richard Dinh <richard.dinh@cfpb.gov>
2022-10-17 14:32:47 -0400

    fix cypress test

--- cypress/e2e/list/list.cy.js
+++ cypress/e2e/list/list.cy.js
@@ -89,13 +89,11 @@ describe('List View', () => {
   describe('Narration buttons', () => {
     it('should filter the results to narrative-only results and back', () => {
       // Initially all is checked.
-      cy.get(addNarrativesButton).should('have.class', 'deselected');
       cy.get(removeNarrativesButton).should('have.class', 'selected');
       cy.get(filterHasNarrative).should('not.be.checked');
 
       // Click the narrative-only button.
       cy.get(addNarrativesButton).click().should('have.class', 'selected');
-      cy.get(removeNarrativesButton).should('have.class', 'deselected');
 
       cy.get(filterHasNarrative).should('be.checked');
 
@@ -103,13 +101,11 @@ describe('List View', () => {
       cy.get(addNarrativesButton)
         .click({ force: true })
         .should('have.class', 'selected');
-      cy.get(removeNarrativesButton).should('have.class', 'deselected');
 
       cy.get(filterHasNarrative).should('be.checked');
 
       // Click the all results button. The narratives should be removed.
       cy.get(removeNarrativesButton).click().should('have.class', 'selected');
-      cy.get(addNarrativesButton).should('have.class', 'deselected');
 
       cy.get(filterHasNarrative).should('not.be.checked');
     });
