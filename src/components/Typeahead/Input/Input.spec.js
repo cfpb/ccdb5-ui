255eee8615573a68aaa627aa55f627299844ac8a
KatherynRizek <katheryn.rizek@cfpb.gov>
2022-10-21 15:02:55 -0400

    Add Input test

--- src/components/Typeahead/Input/Input.spec.js
+++ src/components/Typeahead/Input/Input.spec.js
@@ -1,61 +1,49 @@
 import React from 'react';
-import userEvent from '@testing-library/user-event';
 import {
   testRender as render,
   screen,
   fireEvent,
-  waitFor,
 } from '../../../testUtils/test-utils';
-import { merge } from '../../../testUtils/functionHelpers';
-import { defaultAggs } from '../../../reducers/aggs/aggs';
-import { defaultQuery } from '../../../reducers/query/query';
-import { defaultResults } from '../../../reducers/results/results';
-import { defaultView } from '../../../reducers/view/view';
 import { Input } from './Input';
 
 describe('Input', () => {
+  const handleChangeMock = jest.fn();
   const handleEnterMock = jest.fn();
-  const renderComponent = (handleChangeMock) =>
-    // newAggsState,
-    {
-      // merge(newAggsState, defaultAggs);
-      // const data = {
-      //   aggs: newAggsState,
-      // };
+  const renderComponent = (handleClearMock, isVisible) => {
+    render(
+      <Input
+        ariaLabel="Enter the term you want to search for"
+        htmlId="searchText"
+        handleChange={handleChangeMock}
+        placeholder="Enter your search term(s)"
+        value=""
+        handleClear={handleClearMock}
+        handlePressEnter={handleEnterMock}
+        isClearVisible={isVisible || false}
+      />
+    );
+  };
 
-      render(
-        <Input
-          ariaLabel="Enter the term you want to search for"
-          htmlId="searchText"
-          handleChange={() => handleChangeMock()}
-          placeholder="Enter your search term(s)"
-          value={''}
-          //   handleClear={onClearInput}
-          handlePressEnter={handleEnterMock}
-          isClearVisible={false}
-        />
-      );
-    };
-
-  test('Handle change is called', async () => {
-    // jest.setTimeout(5000);
-    const user = userEvent.setup();
-    const handleChangeMock = jest.fn();
-
-    renderComponent(handleChangeMock);
+  test('Handle when change and enter are called', () => {
+    renderComponent();
     const input = screen.getByRole('textbox');
-    // screen.debug();
-    await user.click(input);
-    // await user.keyboard('value');
+    fireEvent.change(input, { target: { value: 'text' } });
+    expect(handleChangeMock).toBeCalled();
+    fireEvent.click(input);
+    fireEvent.keyDown(input, { key: 'Enter' });
+    expect(handleEnterMock).toBeCalled();
+  });
 
-    // await user.type(
-    //   screen.getByPlaceholderText('Enter your search term(s)'),
-    //   'value'
-    // );
-    screen.debug();
+  test('When handleClear is not given then clear button is not present', () => {
+    renderComponent();
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
 
-    // await waitFor(() => {
-    // expect(handleChangeMock).not.toBeCalled();
-    // });
+  test('When handleClear is  given then clear button is present', () => {
+    const handleClearMock = jest.fn();
+    renderComponent(handleClearMock, true);
+    expect(screen.getByRole('button')).toBeDefined();
+    fireEvent.click(screen.getByRole('button'));
+    expect(handleClearMock).toBeCalled();
   });
 });
