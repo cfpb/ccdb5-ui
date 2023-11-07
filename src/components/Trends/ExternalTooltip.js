aaf556783611b8150f509e533d68cec3001fbad1
Chris Contolini <contolini@users.noreply.github.com>
2023-11-07 15:41:08 -0500

    Add built assets prior to release

--- src/components/Trends/ExternalTooltip.js
+++ src/components/Trends/ExternalTooltip.js
@@ -8,6 +8,12 @@ import React from 'react';
 import { removeFilter } from '../../actions/filter';
 import { sanitizeHtmlId } from '../../utils';
 
+const WARN_SERIES_BREAK =
+  'CFPB updated product and issue options in April 2017 and August 2023.';
+
+const LEARN_SERIES_BREAK =
+  'https://www.consumerfinance.gov/data-research/consumer-complaints/#past-changes';
+
 export class ExternalTooltip extends React.Component {
   _spanFormatter(value) {
     const { focus, lens, hasCompanyTypeahead, subLens } = this.props;
@@ -103,7 +109,16 @@ export class ExternalTooltip extends React.Component {
             )}
           </div>
           <p className="a-micro-copy warn">
-            CFPB updated product and issue options in April 2017 and August 2023
+            {WARN_SERIES_BREAK}{' '}
+            <a
+              href={LEARN_SERIES_BREAK}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Learn more about Product and
+                  Issue changes (opens in new window)"
+            >
+              Learn More
+            </a>
           </p>
         </section>
       );
