5bfc85db978f9dd798998659c0e4f1b6d63998e2
Richard Dinh <richard.dinh@cfpb.gov>
2024-02-06 12:34:27 -0500

    Updated Advanced Search Tips (#459)
    * working mouseover tooltips, updated input placeholder text
    
    * lookin good
    
    * fix text
    
    * unit tests
    
    * unit tests
    
    * Addressing feedback
    
    * add external link icon from design system
    
    * update node to v20
    
    * forgot link
    
    ---------
    
    Co-authored-by: Ans <anselmbradford@users.noreply.github.com>

---
+++ src/components/Search/AdvancedTips/ComplexExample.js
@@ -0,0 +1,87 @@
+import iconMap from '../../iconMap';
+import { TooltipWrapper } from '../../Common/TooltipWrapper/TooltipWrapper';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './ComplexExample.less';
+
+export const ComplexExample = ({ id, notes, placeholderText, tooltipText }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="o-expandable complex-example">
+      <button
+        className={`o-expandable_header o-expandable_target o-expandable_target__${
+          isOpen ? 'expanded' : 'collapsed'
+        }`}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        title="Expand content"
+      >
+        <h3 className="h4 o-expandable_label">
+          Additional information and examples
+        </h3>
+        <span className="o-expandable_link o-expandable_cues">
+          {isOpen ? (
+            <span className="o-expandable_cue o-expandable_cue-close">
+              <span className="u-visually-hidden-on-mobile">
+                {iconMap.getIcon('up')}
+              </span>
+            </span>
+          ) : (
+            <span className="o-expandable_cue o-expandable_cue-open">
+              <span className="u-visually-hidden-on-mobile">
+                {iconMap.getIcon('down')}
+              </span>
+            </span>
+          )}
+        </span>
+      </button>
+      {isOpen ? (
+        <div className="o-expandable_content">
+          {tooltipText && placeholderText ? (
+            <>
+              <label className="descriptor">Complex example:</label>
+              <label className="u-visually-hidden" htmlFor={`example-${id}`}>
+                {tooltipText}
+              </label>
+              <TooltipWrapper text={tooltipText}>
+                {placeholderText.length > 30 ? (
+                  <textarea
+                    className="a-text-input example-input_full"
+                    id={`example-${id}`}
+                    rows={2}
+                    readOnly
+                    value={placeholderText}
+                  />
+                ) : (
+                  <input
+                    className="a-text-input example-input_full"
+                    id={`example-${id}`}
+                    rows={2}
+                    readOnly
+                    value={placeholderText}
+                  />
+                )}
+              </TooltipWrapper>
+            </>
+          ) : null}
+          <label className="descriptor">Notes:</label>
+          <ul className="m-list">
+            {notes.map((note, index) => (
+              <li className="m-list_item" key={index}>
+                {note}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+ComplexExample.propTypes = {
+  id: PropTypes.string.isRequired,
+  notes: PropTypes.array.isRequired,
+  placeholderText: PropTypes.string,
+  tooltipText: PropTypes.string,
+};
