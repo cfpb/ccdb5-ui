ef8322e7f924a4a155f4c10646adbbad10d1c674
Ans <git@anselmbradford.com>
2024-03-29 17:18:58 -0400

    Upgrade to design-system@0.43.0

--- src/components/Warnings/Error.js
+++ src/components/Warnings/Error.js
@@ -9,7 +9,7 @@ export const ErrorBlock = ({ text }) => (
   >
     {getIcon('error-round')}
     <div className="m-notification_content">
-      <div className="h4 m-notification_message">{text}</div>
+      <div className="m-notification_message">{text}</div>
     </div>
   </div>
 );
