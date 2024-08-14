6738ca6f822d706fadc3d2e88266aa4b88018671
Richard Dinh <richard.dinh@cfpb.gov>
2024-08-14 10:30:01 -0700

    fix test

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,11 +1,12 @@
 // TODO: This and the jest.config.js entry can be removed when enzyme is removed
+/* eslint-disable */
 import { TextEncoder } from 'util';
+global.TextEncoder = TextEncoder;
 
 import Adapter from 'enzyme-adapter-react-16';
 import { configure } from 'enzyme';
 // add custom jest matchers from jest-dom for testing library tests
 import '@testing-library/jest-dom';
-global.TextEncoder = TextEncoder;
 
 configure({ adapter: new Adapter() });
 
