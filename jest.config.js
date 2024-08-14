01938a79b9177786b44651f0e23367e2c4429987
Richard Dinh <richard.dinh@cfpb.gov>
2024-08-14 10:21:06 -0700

    another attepmt to get tests to pass in CI

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-// fix for ReferenceError: TextEncoder is not defined jest
-// we probably can remove this once we remove enzyme
-// build was failing here
-// https://github.com/cfpb/ccdb5-ui/actions/runs/10391052214/job/28775102128?pr=526
-// https://stackoverflow.com/a/69734807/659014
-module.exports = {
-  testEnvironment: 'node',
-};
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,8 +1,10 @@
 // TODO: This and the jest.config.js entry can be removed when enzyme is removed
 import Adapter from 'enzyme-adapter-react-16';
+import { TextEncoder } from 'util';
 import { configure } from 'enzyme';
 // add custom jest matchers from jest-dom for testing library tests
 import '@testing-library/jest-dom';
+global.TextEncoder = TextEncoder;
 
 configure({ adapter: new Adapter() });
 
