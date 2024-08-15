c5e8889667a9ae6b646b507992feceff999d2fac
Chanel Henley <129774390+cdmh219@users.noreply.github.com>
2024-08-15 14:02:08 -0400

    DATAP-1492 Conversion of StickyOptions component from class to functional (#526)
    * Initial conversion of StickyOptions component from class to functional
    
    * Implemented with useMemo() hook
    
    * working stickyoptions without useMemo
    
    * try to fix TextEncoder not defined error
    
    * another attepmt to get tests to pass in CI
    
    * fix test
    
    * fix test
    
    * fix test
    
    * try config file
    
    * try gemini ai suggestion
    
    * revert changes
    
    * downgrade coveralls
    
    * pin older version
    
    * took out reference to slice() method on trackedSelections array, as it's unnecessary here
    
    * Changed initial state to be empty array and object, so that useEffect() can be called each time, increasing unit test coverage.
    
    Also took out reference to add missing cache options, since all options are already present
    
    * Added unit test to assert not rendering selections that aren't in options
    
    * adding utility compare function
    
    ---------
    
    Co-authored-by: cdhenley219 <chanel.henley@cfpb.gov>
    Co-authored-by: Richard Dinh <richard.dinh@cfpb.gov>

---
+++ src/components/Filters/StickyOptions/StickyOptions.spec.js
@@ -0,0 +1,59 @@
+import { testRender as render, screen } from '../../../testUtils/test-utils';
+import userEvent from '@testing-library/user-event';
+import StickyOptions from './StickyOptions';
+
+const fixture = [
+  { key: 'DC', doc_count: 999 },
+  { key: 'MS', doc_count: 99 },
+  { key: 'WA', doc_count: 9 },
+];
+
+const renderComponent = (theProps) => render(<StickyOptions {...theProps} />);
+const user = userEvent.setup({ delay: null });
+
+describe('component::StickyOptions', () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      fieldName: 'foo',
+      options: fixture,
+      selections: [],
+    };
+  });
+
+  test('should render an initially selected option', () => {
+    props.selections = ['DC'];
+    renderComponent(props);
+    expect(screen.getByLabelText('DC')).toBeInTheDocument();
+  });
+
+  test('should render nothing when no selections are made', () => {
+    renderComponent(props);
+    const listElement = screen.getByRole('list');
+    //ul element should have no child elements)
+    expect(listElement.childNodes.length).toBe(0);
+  });
+
+  test('should update properly with an additional selection', async () => {
+    props.selections = ['DC', 'WA'];
+    renderComponent(props);
+    const checkboxes = screen.getAllByRole('checkbox');
+
+    //both options should still be present, regardless of checkbox states
+    checkboxes.forEach(async (checkbox) => {
+      await user.click(checkbox);
+    });
+
+    props.selections.forEach((selection) => {
+      expect(screen.getByLabelText(selection)).toBeInTheDocument();
+    });
+  });
+
+  test('should not render selection when its not part of options', () => {
+    props.selections = ['DC'];
+    props.options = [];
+    renderComponent(props);
+    expect(screen.queryByLabelText('DC')).not.toBeInTheDocument();
+  });
+});
