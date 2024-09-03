eb68d3bfcba50b510a29fea43078fe214298c469
cdhenley219 <chanel.henley@cfpb.gov>
2024-09-03 10:14:28 -0400

    added stubs for initial state tests

--- src/components/Charts/LineChart/LineChart.spec.js
+++ src/components/Charts/LineChart/LineChart.spec.js
@@ -64,102 +64,26 @@ const renderComponent = (
 describe('component: LineChart', () => {
   describe('initial state', () => {
     it('should render Overview chart with data', () => {
-      const trendsData = {
-        colorMap: {
-          'Credit reporting': '#2cb34a',
-          'Debt collection': '#addc91',
-          'Credit card or prepaid card': '#257675',
-          Mortgage: '#9ec4c3',
-          'Checking or savings account': '#0072ce',
-          Complaints: '#ADDC91',
-          'All other products': '#a2a3a4',
-        },
-        dataByTopic: [
-          {
-            topic: 'Credit reporting',
-            topicName: 'Credit reporting',
-            dashed: false,
-            show: true,
-            dates: [
-              { date: '2020-03-01T00:00:00.000Z', value: 17231 },
-              { date: '2020-04-01T00:00:00.000Z', value: 21179 },
-              { date: '2020-05-01T00:00:00.000Z', value: 6868 },
-            ],
-          },
-          {
-            topic: 'Debt collection',
-            topicName: 'Debt collection',
-            dashed: false,
-            show: true,
-            dates: [
-              { date: '2020-03-01T00:00:00.000Z', value: 4206 },
-              { date: '2020-04-01T00:00:00.000Z', value: 4508 },
-              { date: '2020-05-01T00:00:00.000Z', value: 1068 },
-            ],
-          },
-          {
-            topic: 'Credit card or prepaid card',
-            topicName: 'Credit card or prepaid card',
-            dashed: false,
-            show: true,
-            dates: [
-              { date: '2020-03-01T00:00:00.000Z', value: 2435 },
-              { date: '2020-04-01T00:00:00.000Z', value: 3137 },
-              { date: '2020-05-01T00:00:00.000Z', value: 712 },
-            ],
-          },
-          {
-            topic: 'Mortgage',
-            topicName: 'Mortgage',
-            dashed: false,
-            show: true,
-            dates: [
-              { date: '2020-03-01T00:00:00.000Z', value: 2132 },
-              { date: '2020-04-01T00:00:00.000Z', value: 2179 },
-              { date: '2020-05-01T00:00:00.000Z', value: 365 },
-            ],
-          },
-          {
-            topic: 'Checking or savings account',
-            topicName: 'Checking or savings account',
-            dashed: false,
-            show: true,
-            dates: [
-              { date: '2020-03-01T00:00:00.000Z', value: 1688 },
-              { date: '2020-04-01T00:00:00.000Z', value: 2030 },
-              { date: '2020-05-01T00:00:00.000Z', value: 383 },
-            ],
-          },
-        ],
-      };
-      renderComponent(
-        {
-          dataByTopic: trendsData.dataByTopic,
-          colorMap: trendsData.colorMap,
-          ...testTrendsState,
-        },
-        testQueryState,
-        testViewState,
-      );
+      renderComponent();
       expect(screen.getByText('Complaints')).toBeInTheDocument();
     });
 
-    // it('renders as expected with no data', () => {
-    //   const trends = { ...testTrendsState, dateRangeLine: { dataByTopic: [] } };
-    //   renderComponent(trends);
+    it('should render non-Overview chart in print mode, with last line date', () => {});
 
-    //   expect(
-    //     screen.getByText(
-    //       'Cannot display chart. Adjust your date range or date interval.',
-    //     ),
-    //   ).toBeInTheDocument();
-    // });
+    it('should render non-Overview chart in print mode, without last line date', () => {});
 
-    //TBD - test for "Overview" variant
+    it('should render non-Overview chart not in print mode', () => {});
 
-    //TBD - test for non-Overview variant
+    it('should render with expected error message when no data is present', () => {
+      const trends = {};
+      renderComponent(trends);
 
-    //TBD  -  test for different printMode
+      expect(
+        screen.getByText(
+          'Cannot display chart. Adjust your date range or date interval.',
+        ),
+      ).toBeInTheDocument();
+    });
   });
 
   describe('tooltip actions', () => {
