3607ca71fb5378928bb02e3e3305e82895202711
Richard Dinh <richard.dinh@cfpb.gov>
2024-09-23 12:24:33 -0700

    fixing bug in cloneDeep function

--- src/utils/chart.js
+++ src/utils/chart.js
@@ -334,12 +334,9 @@ export const pruneIncompleteStackedAreaInterval = (
   interval,
 ) => {
   const { from: dateFrom, to: dateTo } = dateRange;
-  //Note: this double cloneDeep is intentional to prevent state mutation in updateTrendsTooltip dispatch
-  const dataClone = cloneDeep(data);
-  // eslint-disable-next-line no-warning-comments
+  let filteredData = cloneDeep(data);
   // TODO: switch this to structuredClone when JSDOM fixes the issue
   // https://github.com/jsdom/jsdom/issues/3363
-  let filteredData = JSON.parse(JSON.stringify(dataClone));
   //  need to rebuild and sort dates in memory
   const dates = [...new Set(filteredData.map((datum) => datum.date))];
   dates.sort();
