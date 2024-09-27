96c0c7b4e1e05d0c38244658420a19f8bfca5a6e
Richard Dinh <richard.dinh@cfpb.gov>
2024-09-27 13:15:03 -0700

    tour logic to hide when site not running

--- src/reducers/results/selectors.js
+++ src/reducers/results/selectors.js
@@ -1,2 +1,3 @@
+export const selectResultsActiveCall = (state) => state.results.activeCall;
 export const selectResultsIsLoading = (state) => state.results.isLoading;
 export const selectResultsItems = (state) => state.results.items;
