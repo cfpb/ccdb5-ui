2872237be8f065094e19d258958231c3e09daf4d
Chanel Henley <chanel.henley@cfpb.gov>
2025-01-06 16:49:15 -0500

    Reverted to custom typeahead

--- src/components/Filters/FilterSearch/FilterSearch.js
+++ src/components/Filters/FilterSearch/FilterSearch.js
@@ -1,20 +1,19 @@
 import '../../Typeahead/Typeahead.scss';
-import { /*useEffect, useRef,*/ useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
-// import { Typeahead } from 'react-bootstrap-typeahead';
-// import { AsyncTypeahead } from '../../Typeahead/AsyncTypeahead/AsyncTypeahead';
-import { Typeahead } from '../../Typeahead/Typeahead/Typeahead';
+import { Typeahead } from 'react-bootstrap-typeahead';
+// import { Typeahead } from '../../Typeahead/Typeahead/Typeahead';
 import { filterAdded } from '../../../actions';
 import PropTypes from 'prop-types';
 import { useGetAggregations } from '../../../api/hooks/useGetAggregations';
 import { SLUG_SEPARATOR } from '../../../constants';
 import { normalize } from '../../../utils';
-// import { ClearButton } from '../../Typeahead/ClearButton/ClearButton';
-// import HighlightingOption from 'components/Typeahead/HighlightingOption/HighlightingOption';
-// import getIcon from '../../Common/Icon/iconMap';
+import { ClearButton } from '../../Typeahead/ClearButton/ClearButton';
+import HighlightingOption from 'components/Typeahead/HighlightingOption/HighlightingOption';
+import getIcon from '../../Common/Icon/iconMap';
 
 export const FilterSearch = ({ fieldName }) => {
-  //const ref = useRef();
+  const ref = useRef();
   const dispatch = useDispatch();
 
   const fieldNameNew = fieldName.replace(/_/g, ' ');
@@ -43,16 +42,15 @@ export const FilterSearch = ({ fieldName }) => {
       }
     }
   });
-  //console.log('inputText: ', inputText);
 
-  //const handleClear = () => {
-  //ref.current.clear();
-  //setInputText('');
-  //};
+  const handleClear = () => {
+    ref.current.clear();
+    setInputText('');
+  };
 
-  //const [inputText, setInputText] = useState('');
+  const [inputText, setInputText] = useState('');
   //const starterOptions = [];
-  //console.log('buckets: ', buckets)
+
   const [dropdownOptions, setDropdownOptions] = useState(buckets);
 
   const handleInputChange = (value) => {
@@ -85,17 +83,17 @@ export const FilterSearch = ({ fieldName }) => {
 
   const handleSelections = (selected) => {
     dispatch(filterAdded(fieldName, selected[0].key));
-    //handleClear();
+    handleClear();
   };
 
   // give the input focus when the component renders the first time
-  //useEffect(() => {
-  // ref.current.focus();
-  //}, [ref]);
+  useEffect(() => {
+    ref.current.focus();
+  }, [ref]);
 
   return (
-    <div /*className="typeahead"*/>
-      {/* <div className="o-search-input">
+    <div className="typeahead">
+      <div className="o-search-input">
         <div className="o-search-input__input">
           <label
             aria-label={'Search ' + fieldName}
@@ -103,8 +101,8 @@ export const FilterSearch = ({ fieldName }) => {
             htmlFor={'filter-search' + fieldName}
           >
             {getIcon('search')}
-          </label> */}
-      <Typeahead
+          </label>
+          {/* <Typeahead
         htmlId={'filter-search-' + fieldName}
         ariaLabel={`${fieldNameNew} Filter Menu Input`}
         hasClearButton={true}
@@ -113,38 +111,40 @@ export const FilterSearch = ({ fieldName }) => {
         //handleInputChange={(text) => setInputText(text)}
         options={dropdownOptions}
         placeholder={'Enter name of ' + fieldNameNew}
-      />
-      {/* <Typeahead
+      /> */}
+          <Typeahead
             id={'filter-search' + fieldName}
             minLength={2}
-             className="typeahead-selector"
-             filterBy={['key']}
-             onChange={(selected) => handleSelections(selected)}
-             onInputChange={(text) => setInputText(text)}
-             placeholder={'Enter name of ' + fieldNameNew}
-             labelKey="key"
-             options={buckets}
-             ref={ref}
-             inputProps={{
-               'aria-label': `${fieldNameNew} Filter Menu Input`,
-               className: 'a-text-input a-text-input--full',
-             }}
-             renderMenuItemChildren={(option) => (
-               <li className="typeahead-option body-copy">
-                 {option.key.split(SLUG_SEPARATOR)[0]}
-                 {option.value ? (
-                   <div>
-                     <div className="typeahead-option body-copy">
-                       <HighlightingOption {...option} />
-                     </div>
-                   </div>
-                 ) : null}
-               </li>
-             )}
-           /> 
-          {!!inputText && <ClearButton onClear={handleClear} />} */}
-      {/* </div>
-      </div> */}
+            className="typeahead-selector"
+            filterBy={['key']}
+            onChange={(selected) => handleSelections(selected)}
+            onInputChange={
+              (text) => handleInputChange(text) /*setInputText(text)*/
+            }
+            placeholder={'Enter name of ' + fieldNameNew}
+            labelKey="key"
+            options={dropdownOptions}
+            ref={ref}
+            inputProps={{
+              'aria-label': `${fieldNameNew} Filter Menu Input`,
+              className: 'a-text-input a-text-input--full',
+            }}
+            renderMenuItemChildren={(option) => (
+              <li className="typeahead-option body-copy">
+                {option.key.split(SLUG_SEPARATOR)[0]}
+                {option.value ? (
+                  <div>
+                    <div className="typeahead-option body-copy">
+                      <HighlightingOption {...option} />
+                    </div>
+                  </div>
+                ) : null}
+              </li>
+            )}
+          />
+          {!!inputText && <ClearButton onClear={handleClear} />}
+        </div>
+      </div>
     </div>
   );
 };
