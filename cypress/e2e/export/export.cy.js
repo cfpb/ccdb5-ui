7386b155a7121891a15a36edc25db8fcbaf82727
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2025-08-28 15:51:41 -0700

    consolidate list view test to improve performance

--- cypress/e2e/export/export.cy.js
+++ cypress/e2e/export/export.cy.js
@@ -8,12 +8,9 @@ describe('Complaint export', () => {
   const filteredDataset = 'label[for=dataset_filtered]';
   const exportUriInput = '.heres-the-url input';
 
-  beforeEach(() => {
+  it('sends user to an export link without pagination params', () => {
     cy.visit('?size=10&searchText=debt%20recovery&tab=List');
     waitForLoading();
-  });
-
-  it('sends user to an export link without pagination params', () => {
     cy.get(nextButton).click();
     cy.url().should('include', 'page=2');
     cy.get(currentPage).should('have.text', 'Page 2');
