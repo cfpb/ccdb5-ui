3182ad6857aef3be03502c4cb8416f63349634b0
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2025-08-28 08:08:20 -0700

    fix cypress

--- cypress/e2e/utils/index.js
+++ cypress/e2e/utils/index.js
@@ -1,3 +1,5 @@
 export const waitForLoading = () => {
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(100);
   cy.get('.light-box .loading-box', { timeout: 30000 }).should('not.exist');
 };
