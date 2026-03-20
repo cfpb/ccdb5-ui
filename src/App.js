84d388709c42b1833ae1bcc1ef16789538cc0564
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-03-20 08:44:01 -0700

    remove chart icon, lint
    remove stray overrides file
    
    update dist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './css/App.scss';
 import '@cfpb/design-system-react/index.css';
-import './css/overrides.scss';
 import { ReactElement } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router';
 import { ComplaintDetail } from './components/ComplaintDetail/ComplaintDetail';
