77c3d37b7bd3ff521c4441ee9f0bb0ef4e23062b
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-04-10 16:56:00 -0700

    remove per capita test

--- src/components/Map/TileChartMap/TileChartMap.spec.js
+++ src/components/Map/TileChartMap/TileChartMap.spec.js
@@ -7,7 +7,7 @@ import Highcharts from 'highcharts/highmaps';
 import fetchMock from 'jest-fetch-mock';
 import { viewState } from '../../../reducers/view/viewSlice';
 import { mapResults } from './__fixtures__/mapResults';
-import { GEO_NORM_PER1000, MODE_MAP } from '../../../constants';
+import { MODE_MAP } from '../../../constants';
 import * as analyticsActions from '../../../utils';
 
 describe('TileChartMap', () => {
@@ -120,48 +120,6 @@ describe('TileChartMap', () => {
     expect(screen.getByTestId('tile-chart-map')).not.toHaveClass('print');
     await screen.findByLabelText('FL, value: 580,351.');
     expect(screen.getByLabelText('FL, value: 580,351.')).toBeInTheDocument();
-    const { point } = getTileMapPoint('FL');
-    expect(point).toBeDefined();
-    point.firePointEvent('click', { point });
-    expect(analyticsSpy).toHaveBeenCalledWith('State Event: remove', 'FL');
-    expect(removeStateFilterSpy).toHaveBeenCalledWith({
-      abbr: 'FL',
-      name: 'Florida',
-    });
-  });
-
-  it('removes per capita map filters when state filters exist', async () => {
-    fetchMock.mockResponse(JSON.stringify(mapResults));
-    const analyticsSpy = jest
-      .spyOn(analyticsActions, 'sendAnalyticsEvent')
-      .mockImplementation(() => jest.fn());
-
-    const removeStateFilterSpy = jest
-      .spyOn(filterActions, 'stateFilterRemoved')
-      .mockImplementation(() => jest.fn());
-
-    const newFilters = {
-      dataNormalization: GEO_NORM_PER1000,
-      state: ['FL', 'TX'],
-    };
-
-    const newView = {
-      isPrintMode: false,
-      width: 1000,
-    };
-
-    renderComponent(newFilters, newView);
-    expect(screen.getByTestId('tile-chart-map')).toBeInTheDocument();
-    expect(screen.getByTestId('tile-chart-map')).not.toHaveClass('print');
-    expect(await screen.findByText('FL')).toBeInTheDocument();
-    expect(await screen.findByText('28.62')).toBeInTheDocument();
-    expect(screen.getByLabelText('FL, value: 580,351.')).toBeInTheDocument();
-    expect(screen.getByLabelText('OK, value: 20,067.')).toHaveClass(
-      'deselected',
-    );
-    expect(screen.getByLabelText('FL, value: 580,351.')).toHaveClass(
-      'selected',
-    );
     const { point } = getTileMapPoint('FL');
     expect(point).toBeDefined();
     point.firePointEvent('click', { point });
