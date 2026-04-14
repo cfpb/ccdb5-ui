c9a5540ec0e80363f9335dd12f04ae4aeda53a53
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-04-14 09:16:55 -0700

    update dist

--- src/components/Map/map-state-navigation.js
+++ src/components/Map/map-state-navigation.js
@@ -1,18 +1,11 @@
 import './map-state-navigation.scss';
 import { Button } from '@cfpb/design-system-react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { MODE_LIST } from '../../constants';
-import { selectFiltersState } from '../../reducers/filters/selectors';
 import { tabChanged } from '../../reducers/view/viewSlice';
 
 export const MapStateNavigation = () => {
   const dispatch = useDispatch();
-  const stateFilters = useSelector(selectFiltersState) || [];
-  const hasFilters = stateFilters.length > 0;
-
-  if (!hasFilters) {
-    return null;
-  }
 
   return (
     <section className="map-state-navigation u-mb60">
