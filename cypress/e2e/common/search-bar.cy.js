5e4b693c7437ff3a302cb70c0f374e36b626a9e0
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-08-01 23:52:16 -0700

    Add Cypress coverage for search tips, submit, and hero modal.
    Guard a few stable List-path UI flows with Testing Library queries without expanding Trends or Map e2e coverage.

--- cypress/e2e/common/search-bar.cy.js
+++ cypress/e2e/common/search-bar.cy.js
@@ -5,6 +5,9 @@ import { waitForLoading } from '../utils';
 const searchField = () =>
   cy.findByLabelText('Choose which field will be searched');
 
+const searchInput = () =>
+  cy.findByPlaceholderText('Enter your search term(s)');
+
 const typeAheadRequest =
   '**/data-research/consumer-complaints/search/' +
   'api/v1/_suggest_company/**';
@@ -23,8 +26,8 @@ describe('Search Bar', () => {
 
       cy.log('has no typeahead functionality in All Data');
       cy.intercept(typeAheadRequest, { body: [] }).as('typeahead');
-      cy.findByPlaceholderText('Enter your search term(s)').clear();
-      cy.findByPlaceholderText('Enter your search term(s)').type('bank', {
+      searchInput().clear();
+      searchInput().type('bank', {
         delay: 200,
       });
       cy.findByText('No matches found.').should('not.exist');
@@ -32,8 +35,8 @@ describe('Search Bar', () => {
       cy.log('has no typeahead functionality in Narratives');
       searchField().select('complaint_what_happened');
       waitForLoading();
-      cy.findByPlaceholderText('Enter your search term(s)').clear();
-      cy.findByPlaceholderText('Enter your search term(s)').type('bank', {
+      searchInput().clear();
+      searchInput().type('bank', {
         delay: 200,
       });
       cy.findByText('No matches found.').should('not.exist');
@@ -48,8 +51,8 @@ describe('Search Bar', () => {
       }).as('typeahead');
       searchField().select('company');
       waitForLoading();
-      cy.findByPlaceholderText('Enter your search term(s)').clear();
-      cy.findByPlaceholderText('Enter your search term(s)').type('bank', {
+      searchInput().clear();
+      searchInput().type('bank', {
         delay: 200,
       });
 
@@ -62,6 +65,41 @@ describe('Search Bar', () => {
       cy.findAllByRole('option', {
         name: 'Discover Bank',
       }).should('exist');
+    });
+  });
+
+  describe('Advanced search tips', () => {
+    it('toggles search tips', () => {
+      cy.visit('?tab=List');
+      waitForLoading();
+
+      cy.findByRole('heading', { name: 'Search tips' }).should('not.exist');
+      cy.findByRole('button', { name: 'Show advanced search tips' }).click();
+      cy.findByRole('heading', { name: 'Search tips' }).should('be.visible');
+      cy.findByRole('button', { name: 'Hide advanced search tips' }).click();
+      cy.findByRole('heading', { name: 'Search tips' }).should('not.exist');
+      cy.findByRole('button', {
+        name: 'Show advanced search tips',
+      }).should('be.visible');
+    });
+  });
+
+  describe('Search submit', () => {
+    it('submits an All data search', () => {
+      cy.visit('?tab=List');
+      waitForLoading();
+
+      searchField().select('all');
+      waitForLoading();
+      searchInput().clear();
+      searchInput().type('mortgage');
+      cy.findByRole('button', { name: 'Search' }).click();
+      waitForLoading();
+
+      cy.url().should('include', 'searchText=mortgage');
+      cy.findByRole('heading', { name: /Showing .* complaints/ }).should(
+        'be.visible',
+      );
     });
   });
 });
