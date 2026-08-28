74eae54595bdacda8418685e9f639d2faffc8094
Richard Dinh <1038306+flacoman91@users.noreply.github.com>
2026-08-28 08:45:01 -0700

    make like explorer filter pill panel

--- src/components/search/pill-panel.js
+++ src/components/search/pill-panel.js
@@ -1,6 +1,6 @@
 import './pill-panel.scss';
 import { DATE_RANGE_MIN, knownFilters } from '../../constants';
-import { Button, Heading } from '@cfpb/design-system-react';
+import { Button, Paragraph } from '@cfpb/design-system-react';
 import { selectFiltersRoot } from '../../reducers/filters/selectors';
 import {
   selectQueryDateLastIndexed,
@@ -72,10 +72,10 @@ export const PillPanel = () => {
 
   return (
     <section className="pill-panel">
-      <Heading type="3" className="h4 pill-panel__label">
-        Filters applied:
-      </Heading>
       <ul className="m-tag-group pill-panel__list">
+        <li className="pill-panel__override">
+          <Paragraph>Filters applied:</Paragraph>
+        </li>
         {filters.map((filter) => (
           <Pill
             key={filter.fieldName + filter.value}
@@ -88,10 +88,10 @@ export const PillPanel = () => {
             }
           />
         ))}
-        <li className="pill-panel__clear">
+        <li className="pill-panel__override">
           <Button
             appearance="warning"
-            label="Clear all filters"
+            label="Clear filters"
             isLink
             onClick={() => dispatch(filtersCleared(searchField))}
           />
